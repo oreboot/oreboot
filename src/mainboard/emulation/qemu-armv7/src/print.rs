368018259c0c3f9de7ecea889019003762c62b7b
Ryan O'Leary <ryan@ryanoleary.ca>
2019-06-05 15:16:30 +0200

    Add Memory, SliceReader and Section drivers
    Signed-off-by: Ryan O'Leary <ryan@ryanoleary.ca>

--- src/mainboard/emulation/qemu-armv7/src/print.rs
+++ src/mainboard/emulation/qemu-armv7/src/print.rs
@@ -1,18 +1,20 @@
 use core::fmt;
 
 pub struct WriteTo<'a> {
-    drv: &'a driver::Driver,
+    drv: &'a mut driver::Driver,
 }
 
 impl<'a> WriteTo<'a> {
-    pub fn new(drv: &'a driver::Driver) -> Self {
+    pub fn new(drv: &'a mut driver::Driver) -> Self {
         WriteTo { drv: drv }
     }
 }
 
 impl<'a> fmt::Write for WriteTo<'a> {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        self.drv.write(s.as_bytes());
-        Ok(())
+        match self.drv.pwrite(s.as_bytes(), 0) {
+            Err(_) => Err(fmt::Error),
+            _ => Ok(()),
+        }
     }
 }
