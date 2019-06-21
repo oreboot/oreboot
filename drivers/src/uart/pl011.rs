fea1990267600de618141f6a6b0716f8be9ab8d6
pallaud <plaud@google.com>
2019-06-21 11:45:51 -0700

    Adding baud rate calculation
    Sets baud rate registers based on the table given in the PL011 manual.
    
    Signed-off-by: pallaud <plaud@google.com>

--- drivers/src/uart/pl011.rs
+++ drivers/src/uart/pl011.rs
@@ -14,8 +14,8 @@ struct RegisterBlock {
     UARTILPR: ReadWrite<u32>,
     UARTIBRD: ReadWrite<u32>,
     UARTFBRD: ReadWrite<u32>,
-    UARTLCR_H: ReadWrite<u32>,
-    UARTCR: ReadWrite<u32>,
+    UARTLCR_H: ReadWrite<u32, UARTLCR_H::Register>,
+    UARTCR: ReadWrite<u32, UARTCR::Register>,
     UARTIFLS: ReadWrite<u32>,
     UARTIMSC: ReadWrite<u32>,
     UARTRIS: ReadWrite<u32>,
@@ -52,15 +52,28 @@ impl PL011 {
 
 impl Driver for PL011 {
     fn init(&mut self) {
-        // TODO: actually use the given baud rate
-        // 115200
+        // Based on PL011 baud rate divisor table, set baud rate registers
+        let (high, low) = match self.baudrate {
+            230400 => (0x1, 0x5),
+            115200 => (0x2, 0xB),
+            76800 => (0x3, 0x10),
+            38400 => (0x6, 0x21),
+            14400 => (0x11, 0x17),
+            2400 => (0x68, 0xB),
+            110 => (0x8E0, 0x2F),
+            _ => (0x0, 0x3) // Default values 
+        };
         unsafe {
-            (*self.regs).UARTIBRD.set(0x0);
-            (*self.regs).UARTFBRD.set(0x3);
+            (*self.regs).UARTIBRD.set(high);
+            (*self.regs).UARTFBRD.set(low);
+            // Line control: set 8-bits, FIFO enable
+            (*self.regs).UARTLCR_H.modify(UARTLCR_H::WLEN::WLEN_8);
+            (*self.regs).UARTLCR_H.modify(UARTLCR_H::FEN.val(1));
+            // Control register: RX/TX enable, UART enable
+            (*self.regs).UARTCR.modify(UARTCR::RXE.val(1));
+            (*self.regs).UARTCR.modify(UARTCR::TXE.val(1));
+            (*self.regs).UARTCR.modify(UARTCR::UARTEN.val(1));
         }
-        // 2400
-        //(*self.regs).HIGH = 0x0;
-        //(*self.regs).LOW = 0xBF;
     }
 
     fn pread(&self, data: &mut [u8], _offset: usize) -> Result<usize> {
@@ -124,11 +137,54 @@ register_bitfields! {
         DSR OFFSET(1) NUMBITS(1) [],
         // Clear to send
         CTS OFFSET(0) NUMBITS(1) []
+    ],
+    // Line control register
+    UARTLCR_H [
+        // Send break
+        BRK OFFSET(0) NUMBITS(1) [],
+        // Parity enable
+        PEN OFFSET(1) NUMBITS(1) [],
+        // Even parity select
+        EPS OFFSET(2) NUMBITS(1) [],
+        // Two stop bits select
+        STP2 OFFSET(3) NUMBITS(1) [],
+        // FIFOs Enable
+        FEN OFFSET(4) NUMBITS(1) [],
+        // Word length
+        WLEN OFFSET(5) NUMBITS(2) [
+            WLEN_5 = 0,
+            WLEN_6 = 1,
+            WLEN_7 = 2,
+            WLEN_8 = 3
+        ],
+        // Stick Parity select
+        SPS OFFSET(7) NUMBITS(1) []
+    ],
+    // Control register
+    UARTCR [
+        // UART enable
+        UARTEN OFFSET(0) NUMBITS(1) [],
+        // SIR enable
+        SIREN OFFSET(1) NUMBITS(1) [],
+        // IrDA SIR low power mode
+        SIRLP OFFSET(2) NUMBITS(1) [],
+        // Loop back enable
+        LBE OFFSET(7) NUMBITS(1) [],
+        // Transmit enable
+        TXE OFFSET(8) NUMBITS(1) [],
+        // Receive enable
+        RXE OFFSET(9) NUMBITS(1) [],
+        // Data transmit ready
+        DTR OFFSET(10) NUMBITS(1) [],
+        // Request to send
+        RTS OFFSET(11) NUMBITS(1) [],
+        // Data Carrier Detect
+        Out1 OFFSET(12) NUMBITS(1) [],
+        // Ring Indicator
+        Out2 OFFSET(13) NUMBITS(1) [],
+        // RTS hardware flow control enable
+        RTSEn OFFSET(14) NUMBITS(1) [],
+        // CTS hardware flow control enable
+        CTSEn OFFSET(15) NUMBITS(1) []
     ]
-    // TODO: Integer baud rate register
-    //UARTIBRD,
-    // TODO: Fractional baud rate register
-    //UARTFBRD,
-    // TODO: Control register
-    //UARTCR,
 }
