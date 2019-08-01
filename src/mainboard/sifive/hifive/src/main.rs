64d89e879faf8161163ef6afe70fc9dfa8abc3fe
Ronald G. Minnich <rminnich@gmail.com>
2019-08-01 03:40:38 +0000

    hifive: first commit
    There is no soc, no arch, no uart.
    
    But what's nice is when you do a
    RUST_TARGET_PATH=$(PWD) cargo make -p release
    the right stuff happens in terms of fetching a toolchain.
    So consider this a down payment on what we need to do friday.
    
    I'd suggest we just shoot for hardware. coreboot gives us all the info
    we need and hardware is just more fun.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ src/mainboard/sifive/hifive/src/main.rs
@@ -0,0 +1,26 @@
+#![feature(asm)]
+#![feature(lang_items, start)]
+#![no_std]
+#![no_main]
+#![feature(global_asm)]
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    halt()
+}
+use core::panic::PanicInfo;
+
+pub fn halt() -> ! {
+    loop {
+        // Bug with LLVM marks empty loops as undefined behaviour.
+        // See: https://github.com/rust-lang/rust/issues/28728
+        unsafe { asm!("" :::: "volatile") }
+    }
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    halt()
+}
+
