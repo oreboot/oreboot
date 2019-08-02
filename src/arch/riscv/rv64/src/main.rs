abda0795a3907a39daf9c8cc85fd389f7a26b2c1
Ronald G. Minnich <rminnich@gmail.com>
2019-08-02 01:19:59 +0000

    Still trying to work out the right way to add architectures
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ src/arch/riscv/rv64/src/main.rs
@@ -0,0 +1,24 @@
+#![feature(asm)]
+#![feature(lang_items, start)]
+#![no_std]
+#![no_main]
+#![feature(global_asm)]
+
+use core::panic::PanicInfo;
+
+pub fn halt() -> ! {
+    loop {
+        // Bug with LLVM marks empty loops as undefined behaviour.
+        // See: https://github.com/rust-lang/rust/issues/28728
+        unsafe { asm!("" :::: "volatile") }
+    }
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    halt()
+}
+
+global_asm!(include_str!("bootblock.S"));
+
--- src/mainboard/sifive/hifive/src/main.rs
+++ src/mainboard/sifive/hifive/src/main.rs
@@ -4,24 +4,13 @@
 #![no_main]
 #![feature(global_asm)]
 
+use arch::riscv::rv64::*;
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     halt()
 }
-use core::panic::PanicInfo;
-use soc::sifive::fu540;
+//use soc::sifive::fu540;
+//use arch::riscv::rv64;
 
-pub fn halt() -> ! {
-    loop {
-        // Bug with LLVM marks empty loops as undefined behaviour.
-        // See: https://github.com/rust-lang/rust/issues/28728
-        unsafe { asm!("" :::: "volatile") }
-    }
-}
-
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    halt()
-}
 
