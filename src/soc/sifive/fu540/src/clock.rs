ae106f9013a39de3047f9a0e3a1f78100a4950eb
Ryan O'Leary <ryanoleary@google.com>
2019-08-16 17:03:59 -0700

    Initialize clocks
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- src/soc/sifive/fu540/src/clock.rs
+++ src/soc/sifive/fu540/src/clock.rs
@@ -161,12 +161,14 @@ impl Driver for Clock {
     }
 
     fn pwrite(&mut self, data: &[u8], _offset: usize) -> Result<usize> {
-        let r: usize;
         match data {
-            b"on" => r = 1,
-            _ => r = 0,
+            b"on" => {
+                self.clock_init();
+                Ok(1)
+            },
+            _ =>
+                Ok(0)
         }
-        Ok(r)
     }
 
     fn shutdown(&mut self) {}
@@ -200,15 +202,6 @@ const PRCI_CORECLKSEL_CORECLKSEL: u32 = 1;
 
 /* Clock initialization should only be done in romstage. */
 
-// struct pll_settings {
-// 	unsigned int divr:6;
-// 	unsigned int divf:9;
-// 	unsigned int divq:3;
-// 	unsigned int range:3;
-// 	unsigned int bypass:1;
-// 	unsigned int fse:1;
-// };
-
 impl Clock {
     pub fn new() -> Clock {
         Clock { base: reg::PRCI as usize }
@@ -233,127 +226,29 @@ impl Clock {
         }
     }
 
-    // static void configure_pll(u32 *reg, const  struct pll_settings *s)
-    // {
-    // 	// Write the settings to the register
-    // 	u32 c = read32(reg);
-    // 	clrsetbits_le32(&c, PRCI_PLLCFG_DIVR_MASK
-    // 		| PRCI_PLLCFG_DIVF_MASK | PRCI_PLLCFG_DIVQ_MASK
-    // 		| PRCI_PLLCFG_RANGE_MASK | PRCI_PLLCFG_BYPASS_MASK
-    // 		| PRCI_PLLCFG_FSE_MASK,
-    // 		(s->divr << PRCI_PLLCFG_DIVR_SHIFT)
-    // 		| (s->divf << PRCI_PLLCFG_DIVF_SHIFT)
-    // 		| (s->divq << PRCI_PLLCFG_DIVQ_SHIFT)
-    // 		| (s->range << PRCI_PLLCFG_RANGE_SHIFT)
-    // 		| (s->bypass << PRCI_PLLCFG_BYPASS_SHIFT)
-    // 		| (s->fse << PRCI_PLLCFG_FSE_SHIFT));
-    // 	write32(reg, c);
-
-    // 	// Wait for PLL lock
-    // 	while (!(read32(reg) & PRCI_PLLCFG_LOCK))
-    // 		; /* TODO: implement a timeout */
-    // }
-
-    /*
-     * Set coreclk according to the SiFive FU540-C000 Manual
-     * https://www.sifive.com/documentation/chips/freedom-u540-c000-manual/
-     *
-     * Section 7.1 recommends a frequency of 1.0 GHz (up to 1.5 Ghz is possible)
-     *
-     * Section 7.4.2 provides the necessary values:
-     * For example, to setup COREPLL for 1 GHz operation, program divr = 0 (x1),
-     * divf = 59 (4000 MHz VCO), divq = 2 (/4 Output divider)
-     */
-    // static const  struct pll_settings corepll_settings = {
-    // 	.divr = 0,
-    // 	.divf = 59,
-    // 	.divq = 2,
-    // 	.range = 4,
-    // 	.bypass = 0,
-    // 	.fse = 1,
-    // };
-    /*
-     * Section 7.4.3: DDR and Ethernet Subsystem Clocking and Reset
-     *
-     * Unfortunately the documentation example doesn't match the HiFive
-     * Unleashed board settings.
-     * Configuration values taken from SiFive FSBL:
-     * https://github.com/sifive/freedom-u540-c000-bootloader/blob/master/fsbl/main.c
-     *
-     * DDRPLL is set up for 933 MHz output frequency.
-     * divr = 0, divf = 55 (3730 MHz VCO), divq = 2
-     *
-     * GEMGXLPLL is set up for 125 MHz output frequency.
-     * divr = 0, divf = 59 (4000 MHz VCO), divq = 5
-     */
-    // static const  struct pll_settings ddrpll_settings = {
-    // 	.divr = 0,
-    // 	.divf = 55,
-    // 	.divq = 2,
-    // 	.range = 4,
-    // 	.bypass = 0,
-    // 	.fse = 1,
-    // };
-
-    // static const  struct pll_settings gemgxlpll_settings = {
-    // 	.divr = 0,
-    // 	.divf = 59,
-    // 	.divq = 5,
-    // 	.range = 4,
-    // 	.bypass = 0,
-    // 	.fse = 1,
-    // };
-
     fn init_coreclk(&self) {
-        // switch coreclk to input reference frequency before modifying PLL
         self.ClkSel.write(ClkSel::Sel::HF);
-        //	clrsetbits_le32(&prci->coreclksel, PRCI_CORECLK_MASK,PRCI_CORECLK_HFCLK);
 
         self.Core.set(DefaultCore());
-        //	configure_pll(&prci->corepllcfg0, &corepll_settings);
 
-        // switch coreclk to use corepll
-        //	clrsetbits_le32(&prci->coreclksel, PRCI_CORECLK_MASK, PRCI_CORECLK_CORE_PLL);
         self.ClkSel.write(ClkSel::Sel::Core);
     }
 
     fn init_pll_ddr(&self) {
-        // disable ddr clock output before reconfiguring the PLL
         self.DDR1.write(PLLCfg1::Ctrl::Disable);
 
         self.DDR0.set(DefaultDDR());
-        // configure_pll(&prci->ddrpllcfg0, &ddrpll_settings);
 
         self.DDR1.write(PLLCfg1::Ctrl::Enable);
-        // // enable ddr clock output
-        // setbits_le32(&cfg1, PRCI_DDRPLLCFG1_MASK);
-        // write32(&prci->ddrpllcfg1, cfg1);
     }
 
     fn init_pll_ge(&self) {
-        // disable ddr clock output before reconfiguring the PLL
         self.GE1.write(PLLCfg1::Ctrl::Disable);
 
         self.GE0.set(DefaultGE());
-        // configure_pll(&prci->ddrpllcfg0, &ddrpll_settings);
 
         self.GE1.write(PLLCfg1::Ctrl::Enable);
-        // // enable ddr clock output
-        // setbits_le32(&cfg1, PRCI_DDRPLLCFG1_MASK);
-        // write32(&prci->ddrpllcfg1, cfg1);
     }
-
-    // static void init_gemgxlclk(void)
-    // {
-    // 	u32 cfg1 = read32(&prci->gemgxlpllcfg1);
-    // 	clrbits_le32(&cfg1, PRCI_GEMGXLPPLCFG1_MASK);
-    // 	write32(&prci->gemgxlpllcfg1, cfg1);
-
-    // 	configure_pll(&prci->gemgxlpllcfg0, &gemgxlpll_settings);
-
-    // 	setbits_le32(&cfg1, PRCI_GEMGXLPPLCFG1_MASK);
-    // 	write32(&prci->gemgxlpllcfg1, cfg1);
-    // }
 
     fn clock_init(&self) {
         /*
@@ -376,59 +271,37 @@ impl Clock {
         // get DDR out of reset
         // TODO: clean this up later
         self.DevReset.write(ResetCtl::DDRCtl::Reset);
-        //write32(&prci->devicesresetreg, PRCI_DEVICESRESET_DDR_CTRL_RST_N(1));
 
-        // TODO need a fence MACRO. Can't be a call to a function.
-        // 	// HACK to get the '1 full controller clock cycle'.
         architecture::fence();
-        // 	asm volatile ("fence");
-        // YUCK.
+
         self.DevReset.set(ResetMask(false, false, false, false, true));
-        // 	// get all DDR out of reset
-        // 	write32(&prci->devicesresetreg,
-        // 		PRCI_DEVICESRESET_DDR_CTRL_RST_N(1) |
-        // 		PRCI_DEVICESRESET_DDR_AXI_RST_N(1) |
-        // 		PRCI_DEVICESRESET_DDR_AHB_RST_N(1) |
-        // 		PRCI_DEVICESRESET_DDR_PHY_RST_N(1));
 
-        // 	// HACK to get the '1 full controller clock cycle'.
+        // Required to get the '1 full controller clock cycle'.
         architecture::fence();
-        // 	asm volatile ("fence");
 
-        // 	// These take like 16 cycles to actually propagate. We can't go sending
-        // 	// stuff before they come out of reset. So wait.
-        // 	// TODO: Add a register to read the current reset states, or DDR Control
-        // 	// device?
+        // These take like 16 cycles to actually propagate. We can't go sending
+        // stuff before they come out of reset. So wait.
+        // TODO: Add a register to read the current reset states, or DDR Control
+        // device?
         for i in 0..255 {
             architecture::nop();
         }
-        // 	for (int i = 0; i < 256; i++)
-        // 		asm volatile ("nop");
-        // FIXME need a delay
-
         self.init_pll_ge();
         self.DevReset.set(ResetMask(false, false, false, false, false));
-        // 	write32(&prci->devicesresetreg,
-        // 		PRCI_DEVICESRESET_DDR_CTRL_RST_N(1) |
-        // 		PRCI_DEVICESRESET_DDR_AXI_RST_N(1) |
-        // 		PRCI_DEVICESRESET_DDR_AHB_RST_N(1) |
-        // 		PRCI_DEVICESRESET_DDR_PHY_RST_N(1) |
-        // 		PRCI_DEVICESRESET_GEMGXL_RST_N(1));
 
-        // TODO fence
-        // 	asm volatile ("fence");
+        architecture::fence();
     }
 }
 
 // NASTY. We'll live with it for now.
 const UART_DEVICES: u32 = 2;
 const UART_REG_DIV: u32 = 0x18;
-const UART_DIV_VAL: u32 = 4;
+const UART_DIV_VAL: u32 = 4340;
 
 const SPI_DIV: u32 = 0x00;
 const SPI_DIV_VAL: u32 = 4;
 use core::ptr;
-fn poke(v: u32, a: u32) -> () {
+fn poke(a: u32, v: u32) -> () {
     let y = a as *mut u32;
     unsafe {
         ptr::write_volatile(y, v);
@@ -443,348 +316,3 @@ fn update_peripheral_clock_dividers() {
         poke((reg::uart(i) + UART_REG_DIV), UART_DIV_VAL);
     }
 }
-
-//fn clock_init(&self) {
-// void clock_init(void)
-// {
-// 	/*
-// 	 * Update the peripheral clock dividers of UART, SPI and I2C to safe
-// 	 * values as we can't put them in reset before changing frequency.
-// 	 */
-// 	update_peripheral_clock_dividers();
-
-// 	self.init_coreclk();
-
-// 	// put DDR and ethernet in reset
-// 	write32(&prci->devicesresetreg, 0);
-
-// 	init_pll_ddr();
-
-// 	// The following code and its comments is mostly derived from the SiFive
-// 	// u540 bootloader.
-// 	// https://github.com/sifive/freedom-u540-c000-bootloader
-
-// 	// get DDR out of reset
-// 	write32(&prci->devicesresetreg, PRCI_DEVICESRESET_DDR_CTRL_RST_N(1));
-
-// 	// HACK to get the '1 full controller clock cycle'.
-// 	asm volatile ("fence");
-
-// 	// get DDR out of reset
-// 	write32(&prci->devicesresetreg,
-// 		PRCI_DEVICESRESET_DDR_CTRL_RST_N(1) |
-// 		PRCI_DEVICESRESET_DDR_AXI_RST_N(1) |
-// 		PRCI_DEVICESRESET_DDR_AHB_RST_N(1) |
-// 		PRCI_DEVICESRESET_DDR_PHY_RST_N(1));
-
-// 	// HACK to get the '1 full controller clock cycle'.
-// 	asm volatile ("fence");
-
-// 	// These take like 16 cycles to actually propagate. We can't go sending
-// 	// stuff before they come out of reset. So wait.
-// 	// TODO: Add a register to read the current reset states, or DDR Control
-// 	// device?
-// 	for (int i = 0; i < 256; i++)
-// 		asm volatile ("nop");
-
-// 	init_gemgxlclk();
-
-// 	write32(&prci->devicesresetreg,
-// 		PRCI_DEVICESRESET_DDR_CTRL_RST_N(1) |
-// 		PRCI_DEVICESRESET_DDR_AXI_RST_N(1) |
-// 		PRCI_DEVICESRESET_DDR_AHB_RST_N(1) |
-// 		PRCI_DEVICESRESET_DDR_PHY_RST_N(1) |
-// 		PRCI_DEVICESRESET_GEMGXL_RST_N(1));
-
-// 	asm volatile ("fence");
-// }
-
-// /* Get the core clock's frequency, in KHz */
-// int clock_get_coreclk_khz(void)
-// {
-// 	if (read32(&prci->coreclksel) & PRCI_CORECLK_MASK)
-// 		return FU540_BASE_FQY;
-
-// 	u32 cfg  = read32(&prci->corepllcfg0);
-// 	u32 divr = (cfg & PRCI_PLLCFG_DIVR_MASK)
-// 		>> PRCI_PLLCFG_DIVR_SHIFT;
-// 	u32 divf = (cfg & PRCI_PLLCFG_DIVF_MASK)
-// 		>> PRCI_PLLCFG_DIVF_SHIFT;
-// 	u32 divq = (cfg & PRCI_PLLCFG_DIVQ_MASK)
-// 		>> PRCI_PLLCFG_DIVQ_SHIFT;
-
-// 	printk(BIOS_SPEW, "clk: r=%d f=%d q=%d\n", divr, divf, divq);
-// 	return FU540_BASE_FQY
-// 		* 2 * (divf + 1)
-// 		/ (divr + 1)
-// 		/ (1ul << divq);
-// }
-
-// /* Get the TileLink clock's frequency, in KHz */
-// int clock_get_tlclk_khz(void)
-// {
-// 	/*
-// 	 * The TileLink bus and most peripherals use tlclk, which is coreclk/2,
-// 	 * as input.
-// 	 */
-// 	return clock_get_coreclk_khz() / 2;
-// }
-
-/* 7.3 Memory Map (0x1000_0000–0x1000_0FFF) */
-/* This section presents an overview of the PRCI control and configuration registers. */
-/* Copyright © 2018, SiFive Inc. All rights reserved. 40 */
-/*  Crystal Input Control Register (hfxosccfg) */
-/*  Register Offset */
-/* 0x0 */
-/* Bits */
-/* Field Name */
-/* Attr. */
-/* Rst. */
-/* Description */
-/* [28:0] */
-/*  Reserved */
-/* 29 */
-/* xosc_rdy */
-/* RO */
-/* 0x0 */
-/* Crystal input ready */
-/* 30 */
-/* xosccfg_en */
-/* RW */
-/* 0x1 */
-/* Crystal input enable */
-/*            Table 12: */
-/* Crystal Input Control Register */
-/*  Core PLL Configuration Register (corepllcfg0) */
-/*  Register Offset */
-/* 0x4 */
-/* Bits */
-/* Field Name */
-/* Attr. */
-/* Rst. */
-/* Description */
-/* [5:0] */
-/* divr */
-/* RW */
-/* 0x1 */
-/* PLL reference divider value minus one */
-/* [14:6] */
-/* divf */
-/* RW */
-/* 0x1F */
-/* PLL feedback divider value minus one */
-/* [17:15] */
-/* divq */
-/* RW */
-/* 0x3 */
-/* Log2 of PLL output divider. Valid settings are 1, 2, 3, 4, 5, 6 */
-/* [20:18] */
-/* range */
-/* RW */
-/* 0x0 */
-/* PLL filter range. 3’b100 = 33MHz */
-/* [23:21] */
-/*  Reserved */
-/* 24 */
-/* bypass */
-/* RW */
-/* 0x0 */
-/* PLL bypass */
-/* 25 */
-/* fse */
-/* RW */
-/* 0x1 */
-/* Internal or external input path. Valid setting is 1, internal feedback. */
-/* [30:26] */
-/*  Reserved */
-/* 31 */
-/* lock */
-/* RO */
-/* 0x0 */
-/* PLL locked */
-/*                  Table 13: */
-/* Core PLL Configuration Register */
-/*  DDR PLL Configuration Register (ddrpllcfg0) */
-/*  Register Offset */
-/* 0xC */
-/* Bits */
-/* Field Name */
-/* Attr. */
-/* Rst. */
-/* Description */
-/* [5:0] */
-/* divr */
-/* RW */
-/* 0x1 */
-/* PLL reference divider value minus one */
-/* [14:6] */
-/* divf */
-/* RW */
-/* 0x1F */
-/* PLL feedback divider value minus one */
-/* [17:15] */
-/* divq */
-/* RW */
-/* 0x3 */
-/* Log2 of PLL output divider. Valid settings are 1,2,3,4,5,6 */
-/* [20:18] */
-/* range */
-/* RW */
-/* 0x0 */
-/* PLL filter range. 3’b100 = 33MHz */
-/* [23:21] */
-/*  Reserved */
-/* 24 */
-/* bypass */
-/* RW */
-/* 0x0 */
-/* PLL bypass */
-/* 25 */
-/* fse */
-/* RW */
-/* 0x1 */
-/* Internal or external input path. Valid settings is 1, inter- nal feedback. */
-/* [30:26] */
-/*  Reserved */
-/* 31 */
-/* lock */
-/* RO */
-/* 0x0 */
-/* PLL locked */
-/*                  Table 14: */
-/* DDR PLL Configuration Register */
-
-/* Copyright © 2018, SiFive Inc. All rights reserved. 41 */
-/*  DDR PLL Configuration Register (ddrpllcfg1) */
-/*  Register Offset */
-/* 0x10 */
-/* Bits */
-/* Field Name */
-/* Attr. */
-/* Rst. */
-/* Description */
-/* [23:0] */
-/*  Reserved */
-/* 24 */
-/* cke */
-/* RW */
-/* 0x0 */
-/* PLL clock output enable. Glitch free clock gate after PLL output. 1 enables clock, 0 disables clock */
-/*           Table 15: DDR PLL Configuration Register */
-/*  Gigabit Ethernet PLL Configuration Register (gemgxlpllcfg0) */
-/*  Register Offset */
-/* 0x1C */
-/* Bits */
-/* Field Name */
-/* Attr. */
-/* Rst. */
-/* Description */
-/* [5:0] */
-/* divr */
-/* RW */
-/* 0x1 */
-/* PLL reference divider value minus one */
-/* [14:6] */
-/* divf */
-/* RW */
-/* 0x1F */
-/* PLL feedback divider value minus one */
-/* [17:15] */
-/* divq */
-/* RW */
-/* 0x3 */
-/* Log2 of PLL output divider. Valid settings are 1,2,3,4,5,6 */
-/* [20:18] */
-/* range */
-/* RW */
-/* 0x0 */
-/* PLL filter range. 3’b100 = 33MHz */
-/* [23:21] */
-/*  Reserved */
-/* 24 */
-/* bypass */
-/* RW */
-/* 0x0 */
-/* PLL bypass */
-/* 25 */
-/* fse */
-/* RW */
-/* 0x1 */
-/* Internal or external input path. Valid settings is 1, inter- nal feedback. */
-/* [30:26] */
-/*  Reserved */
-/* 31 */
-/* lock */
-/* RO */
-/* 0x0 */
-/* PLL locked */
-/*                  Table 16: Gigabit Ethernet PLL Configuration Register */
-/*  Gigabit Ethernet PLL Configuration Register (gemgxlpllcfg1) */
-/*  Register Offset */
-/* 0x20 */
-/* Bits */
-/* Field Name */
-/* Attr. */
-/* Rst. */
-/* Description */
-/* [23:0] */
-/*  Reserved */
-/* 24 */
-/* cke */
-/* RW */
-/* 0x0 */
-/* PLL clock output enable. Glitch free clock gate after PLL output. 1 enables clock, 0 disables clock */
-/*           Table 17: Gigabit Ethernet PLL Configuration Register */
-/* Table 18: CORECLK Source Selection Register */
-/*  CORECLK Source Selection Register (coreclksel) */
-/*  Register Offset */
-/* 0x24 */
-/* Bits */
-/* Field Name */
-/* Attr. */
-/* Rst. */
-/* Description */
-/* 0 */
-/* coreclksel */
-/* RW */
-/* 0x1 */
-/* CORECLK select. 0 = CORE_PLL output 1 = HFCLK */
-/* [31:1] */
-/*  Reserved */
-
-/* Copyright © 2018, SiFive Inc. All rights reserved. 42 */
-/* Peripheral Devices Reset Control Register (devicesresetreg) */
-/* Register Offset */
-/* 0x28 */
-/* Bits */
-/* Field Name */
-/* Attr. */
-/* Rst. */
-/* Description */
-/* 0 */
-/* DDR_CTRL_RST_N */
-/* RW */
-/* 0x0 */
-/* DDR Controller reset (active low) */
-/* 1 */
-/* DDR_AXI_RST_N */
-/* RW */
-/* 0x0 */
-/* DDR Controller AXI interface reset (active low) */
-/* 2 */
-/* DDR_AHB_RST_N */
-/* RW */
-/* 0x0 */
-/* DDR Controller AHB interface reset (active low) */
-/* 3 */
-/* DDR_PHY_RST_N */
-/* RW */
-/* 0x0 */
-/* DDR PHY reset (active low) */
-/* 4 */
-/* Reserved */
-/* 5 */
-/* GEMGXL_RST_N */
-/* RW */
-/* 0x0 */
-/* Gigabit Ethernet Subsystem reset (active low) */
