7c8e669bff99a5b45912f9370fb8db69616876e6
Gan Shun Lim <ganshun@gmail.com>
2019-08-22 13:02:37 -0700

    add ddr to sifive
    Signed-off-by: Gan Shun Lim <ganshun@gmail.com>

--- src/mainboard/sifive/hifive/src/main.rs
+++ src/mainboard/sifive/hifive/src/main.rs
@@ -6,20 +6,21 @@
 
 mod print;
 
-use heapless::consts::*;
-use heapless::Vec;
+// use heapless::consts::*;
+// use heapless::Vec;
 
 use core::fmt;
 use core::fmt::Write;
 use model::Driver;
 use soc::clock::Clock;
-use uart::log::Log;
+use soc::ddr::DDR;
+// use uart::log::Log;
 use uart::sifive::SiFive;
 use wrappers::DoD;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let mut v: Vec<u8, U1024> = Vec::new();
+    // let v: Vec<u8, U1024> = Vec::new();
     let mut uarts = [&mut SiFive::new(/*soc::UART0*/ 0x10010000, 115200) as &mut dyn Driver];
     let console = &mut DoD::new(&mut uarts[..]);
     console.init();
@@ -28,9 +29,13 @@ pub extern "C" fn _start() -> ! {
     let w = &mut print::WriteTo::new(console);
     fmt::write(w, format_args!("{} {}\r\n", "Formatted output:", 7)).unwrap();
 
+    w.write_str("Initializing clocks.\r\n").unwrap();
     let mut clk = Clock::new();
     clk.pwrite(b"on", 0).unwrap();
 
-    w.write_str("TESTTESTTEST\r\n").unwrap();
+    w.write_str("Clocks initialized!\r\n").unwrap();
+    let mut ddr = DDR::new();
+    ddr.pwrite(b"on", 0).unwrap();
+
     architecture::halt()
 }
