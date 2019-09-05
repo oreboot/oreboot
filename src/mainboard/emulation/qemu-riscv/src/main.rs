e30cd60b7eba302763e5776d9bd6d4fe46eb0633
Alistair Francis <alistair.francis@wdc.com>
2019-09-05 14:50:36 -0700

    riscv/virt: Add support for the RISC-V virt machine
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

---
+++ src/mainboard/emulation/qemu-riscv/src/main.rs
@@ -0,0 +1,64 @@
+#![feature(asm)]
+#![feature(lang_items, start)]
+#![no_std]
+#![no_main]
+#![feature(global_asm)]
+
+use core::fmt::Write;
+use core::panic::PanicInfo;
+use model::Driver;
+use print;
+use payloads::payload;
+use uart::ns16550::NS16550;
+use wrappers::{Memory, SectionReader, SliceReader};
+
+global_asm!(include_str!("bootblock.S"));
+global_asm!(include_str!("init.S"));
+
+#[no_mangle]
+pub extern "C" fn _start(fdt_address: usize) -> ! {
+    let uart0 = &mut NS16550::new(0x10000000, 115200);
+    uart0.init();
+    uart0.pwrite(b"Welcome to oreboot\r\n", 0).unwrap();
+
+    let w = &mut print::WriteTo::new(uart0);
+
+    let kernel_segs = &[
+        payload::Segment {
+            typ: payload::stype::PAYLOAD_SEGMENT_ENTRY,
+            base: 0x80000000,
+            data: &mut SectionReader::new(&Memory {}, 0x20000000 + 0x400000, 6 * 1024 * 1024),
+        },
+        payload::Segment {
+            typ: payload::stype::PAYLOAD_SEGMENT_DATA,
+            base: fdt_address,
+            data: &mut SliceReader::new(&[0u8; 0]),
+        },
+    ];
+    let payload = payload::Payload {
+        typ: payload::ftype::CBFS_TYPE_RAW,
+        compression: payload::ctype::CBFS_COMPRESS_NONE,
+        offset: 0,
+        load_addr: 0x80000000 as u64,
+        rom_len: 0 as u32,
+        mem_len: 0 as u32,
+        segs: kernel_segs,
+    };
+
+    write!(w, "Running payload\r\n").unwrap();
+    payload.run();
+
+    write!(w, "Unexpected return from payload\r\n").unwrap();
+    architecture::halt()
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    // Assume that uart0.init() has already been called before the panic.
+    let uart0 = &mut NS16550::new(0x10000000, 115200);
+    let w = &mut print::WriteTo::new(uart0);
+    // Printing in the panic handler is best-effort because we really don't want to invoke the panic
+    // handler from inside itself.
+    let _ = write!(w, "PANIC: {}\r\n", info);
+    architecture::halt()
+}
