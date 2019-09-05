6347c954b505a49759de6eecb24c9f93552fb402
Gan Shun Lim <ganshun@gmail.com>
2019-09-05 14:25:53 -0700

    Move soc specific stack pointer to new assembly file in soc.
    Signed-off-by: Gan Shun Lim <ganshun@gmail.com>

--- src/mainboard/sifive/hifive/src/main.rs
+++ src/mainboard/sifive/hifive/src/main.rs
@@ -18,6 +18,9 @@ use spi::SiFiveSpi;
 use uart::sifive::SiFive;
 use wrappers::{Memory, SectionReader, SliceReader};
 
+global_asm!(include_str!("../../../../../src/arch/riscv/rv64/src/bootblock.S"));
+global_asm!(include_str!("../../../../../src/soc/sifive/fu540/src/init.S"));
+
 // TODO: For some reason, on hardware, a1 is not the address of the dtb, so we hard-code the device
 // tree here. TODO: The kernel ebreaks when given this device tree.
 const DTB: &'static [u8] = include_bytes!("hifive.dtb");
