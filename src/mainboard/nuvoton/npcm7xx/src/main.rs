48ea8500cfcae379f8085307ef77c9f7f03299db
Ronald G. Minnich <rminnich@gmail.com>
2019-09-25 14:56:55 +0000

    npcm7xx skeleton
    It now builds. Rust changed the target architecture name.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ src/mainboard/nuvoton/npcm7xx/src/main.rs
@@ -0,0 +1,27 @@
+#![feature(asm)]
+#![feature(lang_items, start)]
+#![no_std]
+#![no_main]
+#![feature(global_asm)]
+#![deny(warnings)]
+
+//use core::fmt;
+//use core::fmt::Write;
+//use model::Driver;
+//use print;
+//use wrappers::DoD;
+
+// global_asm!(include_str!("../../../../../src/soc/sifive/fu540/src/bootblock.S"));
+// global_asm!(include_str!("../../../../../src/soc/sifive/fu540/src/init.S"));
+
+#[no_mangle]
+pub extern "C" fn _start(_fdt_address: usize) -> ! {
+    loop{}
+}
+
+// This function is called on panic.
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
