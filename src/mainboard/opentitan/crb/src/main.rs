4ad27f5dc37426c1f2948c4bd20d7079e7c484aa
Ronald G. Minnich <rminnich@gmail.com>
2019-12-13 19:14:32 +0000

    WIP: set up pinmux
    My rust-foo is so bad I figure you can look at this and help.
    
    We want to create a memory device (let m =)
    then write one-word slices to it at offset
    m.pwrite
    
    I'm just putting this POS up there and figure as I revive my
    poor rust memory some of you will just tell me what to do.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/mainboard/opentitan/crb/src/main.rs
+++ src/mainboard/opentitan/crb/src/main.rs
@@ -5,8 +5,8 @@
 #![feature(global_asm)]
 #![deny(warnings)]
 
-use core::panic::PanicInfo;
 use core::fmt::Write;
+use core::panic::PanicInfo;
 use model::Driver;
 use payloads::payload;
 use print;
@@ -15,6 +15,24 @@ use wrappers::{Memory, SectionReader, SliceReader};
 
 #[no_mangle]
 pub extern "C" fn _start_boot_hart(_hart_id: usize, fdt_address: usize) -> ! {
+    // Set up the pinmux. This is highly board dependent.
+    // TODO: pinmux device.
+    let m = &mut Memory;
+
+    m.pwrite(&[0xc2 as u8, 0x40 as u8, 0x14 as u8, 0x06 as u8, ], 0x40070004).unwrap();
+    m.pwrite(&[0x07 as u8, 0x92 as u8, 0x28 as u8, 0x0b as u8, ], 0x40070008).unwrap();
+    m.pwrite(&[0x4c as u8, 0xe3 as u8, 0x3c as u8, 0x10 as u8, ], 0x4007000c).unwrap();
+    m.pwrite(&[0x91 as u8, 0x34 as u8, 0x51 as u8, 0x15 as u8, ], 0x40070010).unwrap();
+    m.pwrite(&[0xd6 as u8, 0x85 as u8, 0x65 as u8, 0x1a as u8, ], 0x40070014).unwrap();
+    m.pwrite(&[0x1b as u8, 0xd7 as u8, 0x79 as u8, 0x1f as u8, ], 0x40070018).unwrap();
+    m.pwrite(&[0x60 as u8, 0x08 as u8, 0x00 as u8, 0x00 as u8, ], 0x4007001c).unwrap();
+    m.pwrite(&[0xc2 as u8, 0x40 as u8, 0x14 as u8, 0x06 as u8, ], 0x40070020).unwrap();
+    m.pwrite(&[0x07 as u8, 0x92 as u8, 0x28 as u8, 0x0b as u8, ], 0x40070024).unwrap();
+    m.pwrite(&[0x4c as u8, 0xe3 as u8, 0x3c as u8, 0x10 as u8, ], 0x40070028).unwrap();
+    m.pwrite(&[0x91 as u8, 0x34 as u8, 0x51 as u8, 0x15 as u8, ], 0x4007002c).unwrap();
+    m.pwrite(&[0xd6 as u8, 0x85 as u8, 0x65 as u8, 0x1a as u8, ], 0x40070030).unwrap();
+    m.pwrite(&[0x1b as u8, 0xd7 as u8, 0x79 as u8, 0x1f as u8, ], 0x40070034).unwrap();
+    m.pwrite(&[0x60 as u8, 0x08 as u8, 0x00 as u8, 0x00 as u8, ], 0x40070038).unwrap();
     let uart0 = &mut OpenTitanUART::new(0x40000000, 115200);
     uart0.init();
     uart0.pwrite(b"Welcome to oreboot\r\n", 0).unwrap();
@@ -56,7 +74,7 @@ pub extern "C" fn _start_boot_hart(_hart_id: usize, fdt_address: usize) -> ! {
 }
 
 #[no_mangle]
-pub extern fn abort() {
+pub extern "C" fn abort() {
     panic!("abort!");
 }
 
