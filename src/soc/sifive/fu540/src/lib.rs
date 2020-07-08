603395c78304feb63e8165050d592618a36978bf
Alistair Francis <alistair.francis@wdc.com>
2020-07-08 11:15:07 -0700

    soc/sifive/fu540: Update QEMU detect logic
    There was another recent QEMU RISC-V ROM change, so let's update Oreboot
    to handle the different values.
    
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- src/soc/sifive/fu540/src/lib.rs
+++ src/soc/sifive/fu540/src/lib.rs
@@ -15,5 +15,5 @@ use core::ptr;
 // TODO: There might be a better way to detect whether we are running in QEMU.
 pub fn is_qemu() -> bool {
     // On hardware, the instruction at 0x1008 is 'lw t1, -4(t0)'.
-    unsafe { ptr::read_volatile(reg::QEMU_FLAG as *mut u32) == 0x01c28593 }
+    unsafe { ptr::read_volatile(reg::QEMU_FLAG as *mut u32) == 0x02828613 }
 }
