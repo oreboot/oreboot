b29d69df0b9791908da7cd48b3ac589c4359279f
Ronald G Minnich <rminnich@gmail.com>
2020-09-11 14:07:24 -0700

    asrock/a300m-stx
    This is just a "forever serial" program and patch.go.
    
    rminnich@rminnich-MacBookPro:~/oreboot/src/mainboard/asrock/a300m-stx$ cat serial.S
    /* this code is position-independent */
    1:
    movb $0x30, %al
    outb %al, 0x80jmp1b//as−oserial.oserial.S//objcopy−Obinaryserial.oserial.binrminnich@rminnich−MacBookPro: /oreboot/src/mainboard/asrock/a300m−stx as -o serial.o serial.S
    rminnich@rminnich-MacBookPro:~/oreboot/src/mainboard/asrock/a300m-stx$ objcopy -O binary serial.o serial.bin
    rminnich@rminnich-MacBookPro:~/oreboot/src/mainboard/asrock/a300m-stx$ ndisasm serial.bin
    00000000 B030 mov al,0x30
    00000002 E680 out 0x80,al
    00000004 EBFA jmp short 0x0
    rminnich@rminnich-MacBookPro:~/oreboot/src/mainboard/asrock/a300m-stx$
    
    First, read the flash part, change it, and put it back.
    sudo ~/flashrom/flashrom -p dediprog:voltage=1.8 -r in.bin
    sudo chmod a-w in.bin
    go run patch.go
    cmp -l in.bin out.bin
    16777201 220 260
    16777202 220 60
    16777203 351 346
    16777204 3 200
    16777205 351 353
    16777206 0 372
    
    the you can flash out.bin and see if it works.
    
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

---
+++ src/mainboard/asrock/a300m-stx/patch.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+var (
+	in  = flag.String("in", "in.bin", "in file")
+	out = flag.String("out", "out.bin", "out file")
+)
+
+func main() {
+	b, err := ioutil.ReadFile(*in)
+	if err != nil {
+		log.Fatal(err)
+	}
+	a := flag.Args()
+	if len(a) == 0 {
+		a = append(a, "serial.bin@FFFFF0")
+	}
+	for _, v := range a {
+		parms := strings.Split(v, "@")
+		if len(parms) < 2 || len(parms) > 3 {
+			log.Fatalf("malformed arg: must be file@off[@size]: %v", v)
+		}
+		f := parms[0]
+		o, err := strconv.ParseUint(parms[1], 16, 24)
+		if err != nil {
+			log.Fatal(err)
+		}
+		off := int(o)
+		patch, err := ioutil.ReadFile(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(patch) > 0x800000 {
+			log.Fatalf("Patch is more than 8m -- please fix")
+		}
+		if off > len(b) {
+			log.Fatalf("Off %d is > len of file %d", off, len(b))
+		}
+		plen := len(patch)
+		if len(parms) == 3 {
+			l, err := strconv.ParseUint(parms[2], 16, 24)
+			if err != nil {
+				log.Fatal(err)
+			}
+			plen = int(l)
+		}
+		if (off + plen) > len(b) {
+			log.Fatalf("Off %d  + len %d is > len of file %d", off, plen, len(b))
+		}
+		if plen > len(patch) {
+			log.Printf("(warning)Patch will only be %#x bytes, not %#x bytes\n", len(patch), plen)
+			plen = len(patch)
+		}
+		log.Printf("Patch %v at %#x for %#x bytes", f, off, plen)
+		copy(b[off:], patch[:plen])
+	}
+	if err := ioutil.WriteFile(*out, b, 0644); err != nil {
+		log.Fatal(err)
+	}
+}
