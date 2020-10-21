3c2499615cf445b87bd7d354a30e7e4fcee92f65
Tomasz Zurkowski <zurkowski@google.com>
2020-10-21 21:44:08 +0200

    Add Driver::pread_exact method.
    This is similar to std::io::Read::read_exact, that reads the exact
    number of bytes to fill in the input buffers. The current code often
    assumes that the buffer will be fully filled in (especially when reading
    u32 into a 4 byte buffer), this method will make it much easier to write
    it correctly.
    
    Signed-off-by: Tomasz Zurkowski <zurkowski@google.com>

---
+++ src/drivers/model/src/tests.rs
@@ -0,0 +1,60 @@
+use super::*;
+
+pub struct OneByteRead<'a> {
+    buf: &'a [u8],
+}
+
+impl<'a> Driver for OneByteRead<'a> {
+    fn pread(&self, data: &mut [u8], pos: usize) -> Result<usize> {
+        if pos >= self.buf.len() {
+            return EOF;
+        }
+        if data.len() == 0 {
+            return Ok(0);
+        }
+        data[0] = self.buf[pos];
+        Ok(1)
+    }
+    fn pwrite(&mut self, _data: &[u8], _pos: usize) -> Result<usize> {
+        Err("not implemented")
+    }
+    fn shutdown(&mut self) {}
+}
+
+#[test]
+fn pread_exact_reads_all_bytes() {
+    let data: [u8; 4] = [1, 2, 3, 4];
+    let drv = OneByteRead { buf: &data };
+
+    let mut got: [u8; 3] = [0, 0, 0];
+    drv.pread_exact(&mut got, 0).unwrap();
+    assert_eq!(data[0..3], got);
+}
+
+#[test]
+fn pread_exact_reads_all_bytes_from_custom_position() {
+    let data: [u8; 4] = [1, 2, 3, 4];
+    let drv = OneByteRead { buf: &data };
+
+    let mut got: [u8; 3] = [0, 0, 0];
+    drv.pread_exact(&mut got, 1).unwrap();
+    assert_eq!(data[1..4], got);
+}
+
+#[test]
+fn pread_exact_returns_ok_for_empty_data_buf() {
+    let data: [u8; 4] = [1, 2, 3, 4];
+    let drv = OneByteRead { buf: &data };
+
+    let mut got: [u8; 0] = [];
+    drv.pread_exact(&mut got, 10).unwrap();
+}
+
+#[test]
+fn pread_exact_returns_eof_when_not_enough_bytes_available() {
+    let data: [u8; 4] = [1, 2, 3, 4];
+    let drv = OneByteRead { buf: &data };
+
+    let mut got: [u8; 5] = [0, 0, 0, 0, 0];
+    assert_eq!(drv.pread_exact(&mut got, 0).err(), Some("EOF"));
+}
