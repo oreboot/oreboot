c1d69099ac33cd36a5731a14cf4010eb1c307237
Ronald G. Minnich <rminnich@gmail.com>
2020-12-02 17:10:07 +0000

    Move acpi setup_acpi_tables function to the board
    The functions are for the architecture, but the function
    itself is per-board.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/arch/x86/x86_64/src/acpi.rs
+++ src/arch/x86/x86_64/src/acpi.rs
@@ -1,12 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 // from github.com:akaros/vmm-akaros/xhype/xhype
-use crate::consts::x86;
+// https://uefi.org/sites/default/files/resources/ACPI_6_3_May16.pdf
+/* SPDX-License-Identifier: GPL-2.0-only */
+// from github.com:akaros/vmm-akaros/xhype/xhype
+
 use core::mem::size_of;
 use core::ptr;
-use util::round_up_4k;
 // https://uefi.org/sites/default/files/resources/ACPI_6_3_May16.pdf
-
 // Table 5-27 RSDP Structure
 #[repr(packed)]
 #[derive(Default)]
@@ -126,6 +127,22 @@ pub struct AcpiTableFadt {
 
 #[repr(packed)]
 #[derive(Default)]
+pub struct AcpiTableFacs {
+    pub signature: [u8; 4], /* ASCII table signature */
+    pub length: u32,        /* Length of table in bytes, including this header */
+    pub hardware_signature: u32,
+    pub firmware_waking_vector: u32,
+    pub global_lock: u32,
+    pub flags: u32,
+    pub xfirm_waking_vector: u64,
+    pub version: u8,
+    pub reserved: [u8; 3],
+    pub ospm_flags: u32,
+    pub reserved_0: [u8; 24],
+}
+
+#[repr(packed)]
+#[derive(Default)]
 pub struct AcpiTableMadt {
     pub header: AcpiTableHeader, /* Common ACPI table header */
     pub address: u32,            /* Physical address of local APIC */
@@ -134,7 +151,20 @@ pub struct AcpiTableMadt {
 
 #[repr(packed)]
 #[derive(Default)]
-pub struct AcpiSubtableHader {
+pub struct AcpiTableMcfg {
+    pub header: AcpiTableHeader, /* Common ACPI table header */
+    pub address: u32,            /* Physical address of local APIC */
+    pub reserved: u8,
+    pub base_address: u64,
+    pub pci_seg_group: u16,
+    pub start_bus: u8,
+    pub end_bus: u8,
+    pub reserved_0: [u8; 4],
+}
+
+#[repr(packed)]
+#[derive(Default)]
+pub struct AcpiSubtableHeader {
     pub r#type: u8,
     pub length: u8,
 }
@@ -142,7 +172,7 @@ pub struct AcpiSubtableHader {
 #[repr(packed)]
 #[derive(Default)]
 pub struct AcpiMadtLocalApic {
-    pub header: AcpiSubtableHader,
+    pub header: AcpiSubtableHeader,
     pub processor_id: u8, /* ACPI processor id */
     pub id: u8,           /* Processor's local APIC id */
     pub lapic_flags: u32,
@@ -151,7 +181,7 @@ pub struct AcpiMadtLocalApic {
 #[repr(packed)]
 #[derive(Default)]
 pub struct AcpiMadtIoApic {
-    pub header: AcpiSubtableHader,
+    pub header: AcpiSubtableHeader,
     pub id: u8,               /* I/O APIC ID */
     pub reserved: u8,         /* reserved - must be zero */
     pub address: u32,         /* APIC physical address */
@@ -161,7 +191,7 @@ pub struct AcpiMadtIoApic {
 #[repr(packed)]
 #[derive(Default)]
 pub struct AcpiMadtLocalX2apic {
-    pub header: AcpiSubtableHader,
+    pub header: AcpiSubtableHeader,
     pub reserved: u16,      /* reserved - must be zero */
     pub local_apic_id: u32, /* Processor x2APIC ID  */
     pub lapic_flags: u32,
@@ -171,7 +201,7 @@ pub struct AcpiMadtLocalX2apic {
 #[repr(packed)]
 #[derive(Default)]
 pub struct AcpiMadtInterruptOverride {
-    pub header: AcpiSubtableHader,
+    pub header: AcpiSubtableHeader,
     pub bus: u8,
     pub sourceirq: u8,
     pub globalirq: u32,
@@ -181,21 +211,32 @@ pub struct AcpiMadtInterruptOverride {
 #[repr(packed)]
 #[derive(Default)]
 pub struct AcpiMadtNMI {
-    pub header: AcpiSubtableHader,
+    pub header: AcpiSubtableHeader,
     pub flags: u16,
     pub globalirq: u32,
 }
 
 #[repr(packed)]
 #[derive(Default)]
-pub struct AcpiMadtLAPICNMI {
-    pub header: AcpiSubtableHader,
-    pub acpi_processor_uid: u8,
+pub struct AcpiMadtLocalX2ApicNMI {
+    pub header: AcpiSubtableHeader,
     pub flags: u16,
+    pub acpi_processor_uid: u32,
     pub local_interrupt: u8,
+    pub reserved: [u8; 3],
 }
 
-fn write<T>(w: &mut impl core::fmt::Write, val: T, offset: usize, index: usize) {
+#[repr(packed)]
+#[derive(Default)]
+pub struct CfgSpace {
+    pub base_address: u64,
+    pub pci_seg_group: u16,
+    pub start_bus: u8,
+    pub end_bus: u8,
+    pub reserved: [u8; 4],
+}
+
+pub fn write<T>(w: &mut impl core::fmt::Write, val: T, offset: usize, index: usize) {
     let y = (offset + index * size_of::<T>()) as *mut T;
     unsafe {
         ptr::write_volatile(y, val);
@@ -203,7 +244,7 @@ fn write<T>(w: &mut impl core::fmt::Write, val: T, offset: usize, index: usize) 
     write!(w, "write to {:x?}: \r\n", y).unwrap();
 }
 
-fn read<T>(offset: usize, index: usize) -> T {
+pub fn read<T>(offset: usize, index: usize) -> T {
     let y = (offset + index * size_of::<T>()) as *mut T;
     unsafe { ptr::read_volatile(y) }
 }
@@ -247,129 +288,28 @@ pub const DSDT_DSDTTBL_HEADER: [u8; 36] = [
 ];
 
 #[inline]
-fn gencsum(start: usize, end: usize) -> u8 {
+pub fn gencsum(start: usize, end: usize) -> u8 {
     let mut tot: u16 = 0;
-    for i in start..end + 1 {
+    for i in start..end {
         let b: u8 = read(i, 0);
         tot += b as u16;
     }
-    tot as u8
+    (!tot as u8) + 1
 }
 
-// I still don't know why these were different, I thought it was the
-// same algorithm.
 #[inline]
-fn acpi_tb_checksum(start: usize, end: usize) -> u8 {
+pub fn acpi_tb_checksum(start: usize, end: usize) -> u8 {
     gencsum(start, end)
 }
 
-const SIG_RSDP: [u8; 8] = *b"RSD PTR ";
-const SIG_XSDT: [u8; 4] = *b"XSDT";
-const SIG_FADT: [u8; 4] = *b"FACP";
-const SIG_MADT: [u8; 4] = *b"APIC";
+pub const SIG_RSDP: [u8; 8] = *b"RSD PTR ";
+pub const SIG_XSDT: [u8; 4] = *b"XSDT";
+pub const SIG_FADT: [u8; 4] = *b"FACP";
+pub const SIG_FACS: [u8; 4] = *b"FACS";
+pub const SIG_MADT: [u8; 4] = *b"APIC";
+pub const SIG_MCFG: [u8; 4] = *b"MCFG";
 
-const MADT_LOCAL_APIC: u8 = 0;
-const MADT_IO_APIC: u8 = 1;
-const MADT_LOCAL_X2APIC: u8 = 9;
-const MADT_LOCAL_ISOR: u8 = 2;
-// annoying. const MADT_LOCAL_NMI: u8 = 3;
-const MADT_LOCAL_LAPICNMI: u8 = 4;
-
-/// Setup the BIOS tables in the low memory
-///
-/// `start` is the base address of the BIOS tables in the physical address space
-/// of the guest. `low_mem` is a host virtual memory block which is mapped to
-/// the lowest memory of the guest. `cores` is the number of logical CPUs of the guest.
-/// Total number of bytes occupied by the BIOS tables is returned.
-pub fn setup_bios_tables(w: &mut impl core::fmt::Write, start: usize, cores: u32) -> usize {
-    // calculate offsets first
-    // variables with suffix `_offset` mean the offset in `low_mem`. They are
-    // also the guest physical address of the corresponding tables, since `low_mem` is
-    // mapped to the lowest memory of the guest's physical address space.
-
-    let rsdp_offset = start;
-    let xsdt_offset = rsdp_offset + size_of::<AcpiTableRsdp>();
-    let xsdt_entry_offset = xsdt_offset + size_of::<AcpiTableHeader>();
-    const NUM_XSDT_ENTRIES: usize = 8;
-    let fadt_offset = xsdt_entry_offset + NUM_XSDT_ENTRIES * size_of::<u64>();
-    let dsdt_offset = fadt_offset + size_of::<AcpiTableFadt>();
-    let madt_offset = dsdt_offset + DSDT_DSDTTBL_HEADER.len();
-    let madt_local_apic_offset = madt_offset + size_of::<AcpiTableMadt>();
-    let io_apic_offset = madt_local_apic_offset + cores as usize * size_of::<AcpiMadtLocalApic>();
-    let local_x2apic_offset = io_apic_offset + size_of::<AcpiMadtIoApic>();
-    let local_lapicnmi_offset = local_x2apic_offset + cores as usize * size_of::<AcpiMadtLocalX2apic>();
-    let local_isor_offset = local_lapicnmi_offset + size_of::<AcpiMadtLAPICNMI>();
-    let total_size = local_isor_offset + 2 * size_of::<AcpiMadtInterruptOverride>() - start;
-
-    // setup rsdp
-    let rsdp = AcpiTableRsdp { signature: SIG_RSDP, revision: 2, length: 36, xsdt_physical_address: xsdt_offset as u64, ..Default::default() };
-    write!(w, "Write rsdp  at {:x?} \r\n", rsdp_offset).unwrap();
-    write(w, rsdp, rsdp_offset, 0);
-    write(w, gencsum(rsdp_offset, rsdp_offset + ACPI_RSDP_CHECKSUM_LENGTH), rsdp_offset, ACPI_RSDP_CHECKSUM_OFFSET); // XXX
-    debug_assert_eq!(acpi_tb_checksum(rsdp_offset, rsdp_offset + ACPI_RSDP_CHECKSUM_LENGTH), 0);
-    write(w, gencsum(rsdp_offset, rsdp_offset + ACPI_RSDP_XCHECKSUM_LENGTH), rsdp_offset, ACPI_RSDP_XCHECKSUM_OFFSET); // XXX
-    debug_assert_eq!(acpi_tb_checksum(rsdp_offset, rsdp_offset + ACPI_RSDP_XCHECKSUM_LENGTH), 0);
-
-    // xsdt
-    let xsdt_total_length = size_of::<AcpiTableHeader>() + size_of::<u64>() * NUM_XSDT_ENTRIES;
-    let xsdt = AcpiTableHeader { signature: SIG_XSDT, length: xsdt_total_length as u32, ..AcpiTableHeader::new() };
-    write(w, xsdt, xsdt_offset, 0);
-    // xsdt entries
-    let mut xsdt_entries: [u64; NUM_XSDT_ENTRIES] = [0; NUM_XSDT_ENTRIES];
-    xsdt_entries[0] = fadt_offset as u64;
-    xsdt_entries[3] = madt_offset as u64;
-    write(w, xsdt_entries, xsdt_entry_offset, 0);
-    write(w, gencsum(xsdt_offset, xsdt_offset + xsdt_total_length), xsdt_offset, ACPI_TABLE_HEADER_CHECKSUM_OFFSET); // XXX
-    debug_assert_eq!(acpi_tb_checksum(xsdt_offset, xsdt_offset + xsdt_total_length), 0);
-
-    // fadt
-    let fadt = AcpiTableFadt { header: AcpiTableHeader { signature: SIG_FADT, length: size_of::<AcpiTableFadt>() as u32, ..AcpiTableHeader::new() }, xdsdt: dsdt_offset as u64, ..Default::default() };
-    write(w, fadt, fadt_offset, 0);
-    write(w, gencsum(fadt_offset, fadt_offset + size_of::<AcpiTableFadt>()), fadt_offset, ACPI_TABLE_HEADER_CHECKSUM_OFFSET); // XXX
-    debug_assert_eq!(acpi_tb_checksum(fadt_offset, fadt_offset + size_of::<AcpiTableFadt>()), 0);
-
-    // dsdt
-    write(w, DSDT_DSDTTBL_HEADER, dsdt_offset, 0);
-
-    // madt
-    let madt_total_length = size_of::<AcpiTableMadt>() + size_of::<AcpiMadtIoApic>() + cores as usize * (size_of::<AcpiMadtLocalApic>() + size_of::<AcpiMadtLocalX2apic>());
-    let madt = AcpiTableMadt { header: AcpiTableHeader { signature: SIG_MADT, length: madt_total_length as u32, ..AcpiTableHeader::new() }, address: x86::APIC_BASE as u32, flags: 0 };
-    write(w, madt, madt_offset, 0);
-
-    // local apic
-    for i in 0..cores {
-        let lapic = AcpiMadtLocalApic { header: AcpiSubtableHader { r#type: MADT_LOCAL_APIC, length: size_of::<AcpiMadtLocalApic>() as u8 }, processor_id: i as u8, id: i as u8, lapic_flags: 1 };
-        write(w, lapic, madt_local_apic_offset, i as usize)
-    }
-
-    // io apiic
-    let io_apic = AcpiMadtIoApic { header: AcpiSubtableHader { r#type: MADT_IO_APIC, length: size_of::<AcpiMadtIoApic>() as u8 }, id: 0xf0, address: x86::IO_APIC_BASE as u32, global_irq_base: 0, ..Default::default() };
-    write(w, io_apic, io_apic_offset, 0);
-
-    // local x2apic
-    for i in 0..cores {
-        let x2apic = AcpiMadtLocalX2apic { header: AcpiSubtableHader { r#type: MADT_LOCAL_X2APIC, length: size_of::<AcpiMadtLocalX2apic>() as u8 }, local_apic_id: i, uid: i, lapic_flags: 1, ..Default::default() };
-        write(w, x2apic, local_x2apic_offset, i as usize)
-    }
-    write(w, gencsum(madt_offset, madt_offset + madt_total_length), madt_offset, ACPI_TABLE_HEADER_CHECKSUM_OFFSET); // XXX
-
-    // LAPICNMI
-    write!(w, "LAPICNMI\r\n").unwrap();
-    let lapicnmi = AcpiMadtLAPICNMI { header: AcpiSubtableHader { r#type: MADT_LOCAL_LAPICNMI, length: size_of::<AcpiMadtLAPICNMI>() as u8 }, acpi_processor_uid: 0xff, flags: 5, local_interrupt: 1 };
-    write(w, lapicnmi, local_lapicnmi_offset, 0 as usize);
-
-    // isor -- rhymes with eyesore
-    write!(w, "First ISOR\r\n").unwrap();
-    let isor = AcpiMadtInterruptOverride { header: AcpiSubtableHader { r#type: MADT_LOCAL_ISOR, length: size_of::<AcpiMadtInterruptOverride>() as u8 }, bus: 0, sourceirq: 9, globalirq: 9, flags: 0xf };
-    write(w, isor, local_isor_offset, 0 as usize);
-
-    write!(w, "Second ISOR\r\n").unwrap();
-    let isor = AcpiMadtInterruptOverride { header: AcpiSubtableHader { r#type: MADT_LOCAL_ISOR, length: size_of::<AcpiMadtInterruptOverride>() as u8 }, bus: 0, sourceirq: 0, globalirq: 2, flags: 0 };
-    write(w, isor, local_isor_offset, 1 as usize);
-
-    write!(w, "Gencsum from {:x} to {:x} store into {:x}\r\n", madt_offset, madt_offset + madt_total_length, ACPI_TABLE_HEADER_CHECKSUM_OFFSET).unwrap();
-    write(w, gencsum(madt_offset, madt_offset + madt_total_length), madt_offset, ACPI_TABLE_HEADER_CHECKSUM_OFFSET); // XXX
-    debug_assert_eq!(acpi_tb_checksum(madt_offset, madt_offset + madt_total_length), 0);
-
-    round_up_4k(total_size)
-}
+pub const MADT_LOCAL_APIC: u8 = 0;
+pub const MADT_IO_APIC: u8 = 1;
+pub const MADT_LOCAL_X2APIC: u8 = 9;
+pub const MADT_LOCAL_ISOR: u8 = 2;
