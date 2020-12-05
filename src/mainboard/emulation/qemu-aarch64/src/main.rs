4c72a66c234d2dc9ad2d4a2f6dec37c68ec4c7e0
ichihlu <ichihlu@google.com>
2020-12-05 12:43:45 +0800

    Support QEMU aarch64 virt platform.
    Draft version cloned from QEMU armv7 platform.
    
    Signed-off-by: ichihlu <ichihlu@google.com>

---
+++ src/mainboard/emulation/qemu-aarch64/src/main.rs
@@ -0,0 +1,62 @@
+#![feature(llvm_asm)]
+#![feature(lang_items, start)]
+#![no_std]
+#![no_main]
+#![feature(global_asm)]
+#![deny(warnings)]
+
+mod romstage;
+use core::fmt::Write;
+
+use device_tree::print_fdt;
+use model::Driver;
+use payloads::external::zimage::DTB;
+use uart;
+use wrappers::{DoD, SliceReader};
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let mut pl011 = uart::pl011::PL011::new(0x09000000, 115200);
+    let uart_driver: &mut dyn Driver = &mut pl011;
+    // TODO: Handle error here and quit, rather than unwrapping.
+    uart_driver.init().unwrap();
+    uart_driver.pwrite(b"Welcome to oreboot\r\n", 0).unwrap();
+    let s = &mut [uart_driver];
+    let console = &mut DoD::new(s);
+    let mut w = print::WriteTo::new(console);
+    let spi = SliceReader::new(DTB);
+
+    if let Err(err) = print_fdt(&spi, &mut w) {
+        write!(w, "error: {}\n", err).expect(err);
+    }
+
+    cpu::init();
+    let mut w = print::WriteTo::new(console);
+    write!(w, "hi").expect("blame ryan");
+    write!(w, "1").expect("blame ryan");
+    write!(w, "2").expect("blame ryan");
+    write!(w, "3").expect("blame ryan");
+    write!(w, "4").expect("blame ryan");
+    write!(w, "5").expect("blame ryan");
+    write!(w, "6").expect("blame ryan");
+    write!(w, "7").expect("blame ryan");
+    write!(w, "{}{}\r\n", 3, "7").expect("blame ryan");
+    romstage::romstage()
+}
+use core::panic::PanicInfo;
+
+pub fn halt() -> ! {
+    loop {
+        // Bug with LLVM marks empty loops as undefined behaviour.
+        // See: https://github.com/rust-lang/rust/issues/28728
+        unsafe { llvm_asm!("" :::: "volatile") }
+    }
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    halt()
+}
+
+global_asm!(include_str!("vector_table.S"));
