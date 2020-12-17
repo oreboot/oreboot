b209ac67c2b0ed5a4b562ce00c7ffc8246d65484
Titus Rwantare <titusr@google.com>
2020-12-16 16:13:23 -0800

    remove shifts and use 16bit reads

--- src/arch/x86/x86_64/src/pci.rs
+++ src/arch/x86/x86_64/src/pci.rs
@@ -2,7 +2,7 @@ use model::*;
 
 /* Device config space registers. */
 const REG_VENDOR_ID: u16 = 0x00;
-// const REG_DEVICE_ID: u16 = 0x02;
+const REG_DEVICE_ID: u16 = 0x02;
 // const REG_COMMAND: u16 = 0x04;
 // const REG_STATUS: u16 = 0x06;
 // const REG_REVISION_ID: u16 = 0x08;
@@ -11,7 +11,7 @@ const REG_VENDOR_ID: u16 = 0x00;
 // const REG_CLASS: u16 = 0x0B;
 // const REG_CACHE_LINE_SIZE: u16 = 0x0C;
 // const REG_LATENCY_TIMER: u16 = 0x0D;
-// const REG_HEADER_TYPE: u16 = 0x0E;
+const REG_HEADER_TYPE: u16 = 0x0E;
 // const REG_BIST: u16 = 0x0F;
 // const REG_BAR0: u16 = 0x10;
 // const REG_BAR1: u16 = 0x14;
@@ -30,8 +30,8 @@ const REG_VENDOR_ID: u16 = 0x00;
 // const REG_MAX_LATENCY: u16 = 0x3F;
 
 /* Bridge config space registers. */
-const REG_PRIMARY_BUS: u16 = 0x18;
-// const REG_SECONDARY_BUS: u16 = 0x19;
+// const REG_PRIMARY_BUS: u16 = 0x18;
+const REG_SECONDARY_BUS: u16 = 0x19;
 // const REG_SUBORDINATE_BUS: u16 = 0x1A;
 // const REG_SECONDARY_LATENCY: u16 = 0x1B;
 // const REG_IO_BASE: u16 = 0x1C;
@@ -53,37 +53,39 @@ unsafe fn outl(port: u16, val: u32) {
     llvm_asm!("outl %eax, %dx" :: "{dx}"(port), "{al}"(val));
 }
 
-/// Read 32 bits from port
-unsafe fn inl(port: u16) -> u32 {
-    let ret: u32;
-    llvm_asm!("inl %dx, %eax" : "={ax}"(ret) : "{dx}"(port) :: "volatile");
+/// Write 16 bits to port
+// unsafe fn outw(port: u16, val: u16) {
+//     llvm_asm!("outw %ax, %dx" :: "{dx}"(port), "{al}"(val));
+// }
+
+// /// Read 16 bits from port
+unsafe fn inw(port: u16) -> u16 {
+    let ret: u16;
+    llvm_asm!("inw %dx, %ax" : "={ax}"(ret) : "{dx}"(port) :: "volatile");
     ret
 }
-
-fn pci_read_config(device: u32, register: u16) -> Result<u32> {
+fn pci_read_config16(device: u32, register: u16) -> Result<u16> {
     unsafe {
         outl(0xCF8, device | (register & !3) as u32);
-        Ok(inl(0xCFC + (register & 3)))
+        Ok(inw(0xCFC + (register & 3)))
     }
 }
 
-pub fn scan_bus(w: &mut impl core::fmt::Write, bus: u8) {
+pub fn scan_bus(w: &mut impl core::fmt::Write, bus: u16) {
     for devfn in 0..0x100 {
         let device = (devfn >> 3) & 0x1F;
         let function = devfn & 0x7;
 
         let port_address = 0x8000_0000 | device << 11 | function << 8;
-        let value = pci_read_config(port_address, REG_VENDOR_ID).unwrap();
+        let ven_id= pci_read_config16(port_address, REG_VENDOR_ID).unwrap();
+        let dev_id= pci_read_config16(port_address, REG_DEVICE_ID).unwrap();
 
-        if value == 0xFFFF || value == 0xFFFF_0000 || value == 0xFFFF_FFFF || value == 0 {
+        if dev_id == 0xFFFF || ven_id == 0xFFFF || dev_id == 0 {
             continue;
         }
 
-        let hdr_type = pci_read_config(port_address, 0xC).unwrap();
-        let hdr_type = (hdr_type >> 16) & 0x7F;
-        
-        let ven_id = value & 0xFFFF;
-        let dev_id = (value >> 8) & 0xFFFF;
+        let hdr_type = pci_read_config16(port_address, REG_HEADER_TYPE).unwrap();
+        let hdr_type = hdr_type & 0x7F;
 
         if hdr_type == 0 {
             writeln!(w, "PCI: Found PCI device at {:#04x}:{:x}:{:x}\r", bus, device, function).unwrap();
@@ -97,8 +99,8 @@ pub fn scan_bus(w: &mut impl core::fmt::Write, bus: u8) {
             writeln!(w, "PCI: Found PCI cardbus at {:#04x}:{:x}:{:x}\r", bus, device, function).unwrap();
         }
         if hdr_type == 1 || hdr_type == 2 {
-            let next_bus = pci_read_config(port_address, REG_PRIMARY_BUS).unwrap();
-            let next_bus = ((next_bus >> 8) & 0xFF) as u8;
+            let next_bus = pci_read_config16(port_address, REG_SECONDARY_BUS).unwrap();
+            let next_bus = next_bus & 0xFF;
 
             if next_bus != bus {
                 scan_bus(w, next_bus);
@@ -107,6 +109,5 @@ pub fn scan_bus(w: &mut impl core::fmt::Write, bus: u8) {
         }
         writeln!(w, "PCI: Found header {:X} at {:04X}:{:02X}:{:X}\r", hdr_type, bus, device, function).unwrap();
         writeln!(w, "PCI: \tVendor ID: {:#06X} Device ID: {:#06X}\r", ven_id, dev_id).unwrap();
-
     }
 }
