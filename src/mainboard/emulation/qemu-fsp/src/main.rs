47a750755ad4dfcb6db9f700e3ad8fcacb44a811
Ryan O'Leary <ryanoleary@google.com>
2021-01-04 17:03:42 -0800

    Run FSP-M in qemu
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- src/mainboard/emulation/qemu-fsp/src/main.rs
+++ src/mainboard/emulation/qemu-fsp/src/main.rs
@@ -10,6 +10,13 @@ use core::panic::PanicInfo;
 use model::Driver;
 use print;
 use uart::i8250::I8250;
+use uefi::{fv_traverse, SectionData};
+
+use heapless::consts::U4;
+use heapless::Vec;
+
+use efi::EFI_GUID as GUID;
+use fsp_qemu_sys as efi;
 
 // Unless we mention the fsp_qemu_sys crate, the compiler will optimize it away. This crate
 // introduces the symbols containing the FSP binary which get picked up by the linker.
@@ -30,6 +37,67 @@ pub extern "C" fn _start(_fdt_address: usize) -> ! {
 
     let w = &mut print::WriteTo::new(uart0);
 
+    let fsp_base = 0xFFF80000usize;
+    let fsp_size = 0x40000usize;
+    let fspfv = unsafe { core::slice::from_raw_parts(fsp_base as *const u8, fsp_size) };
+    let infos = match find_fsp(fspfv) {
+        Ok(x) => x,
+        Err(err) => panic!("Error finding FSP: {}\r\n", err),
+    };
+    write!(w, "Found FSP_INFO: {:#x?}\r\n", infos).unwrap();
+
+    if let Some(fspm_entry) = fsp_memory_init_entry(&infos) {
+        write!(
+            w,
+            "Calling FspMemoryInit at {:#x}+{:#x}\r\n",
+            fsp_base, fspm_entry
+        )
+        .unwrap();
+
+        // TODO: This struct has to be aligned to 4.
+        // mut because we can't make the assumption FSP won't modify it.
+        let mut fspm_upd = efi::FSPM_UPD {
+            FspUpdHeader: efi::FSP_UPD_HEADER {
+                Signature: efi::FSPM_UPD_SIGNATURE,
+                Revision: 2, // FSP 2.2
+                Reserved: [0u8; 23],
+            },
+            FspmArchUpd: efi::FSPM_ARCH_UPD {
+                Revision: 2, // FSP 2.2
+                Reserved: [0u8; 3],
+                NvsBufferPtr: 0,        // non-volatile storage not available
+                StackBase: 0x20000000,  // TODO: I picked this at random
+                StackSize: 0x10000,     // TODO: I picked this at random
+                BootLoaderTolumSize: 0, // Don't reserve "top of low usable memory" for bootloader.
+                BootMode: efi::BOOT_WITH_FULL_CONFIGURATION,
+                FspEventHandler: 0 as *mut efi::FSP_EVENT_HANDLER, // optional
+                Reserved1: [0u8; 4],
+            },
+            FspmConfig: efi::FSP_M_CONFIG {
+                SerialDebugPortAddress: 0x3f8,
+                SerialDebugPortType: 1,       // I/O
+                SerialDebugPortDevice: 3,     // External Device
+                SerialDebugPortStrideSize: 0, // 1
+                UnusedUpdSpace0: [0; 49],
+                ReservedFspmUpd: [0; 4],
+            },
+            UnusedUpdSpace1: [0u8; 2],
+            UpdTerminator: 0x55AA, // ???
+        };
+
+        let status = unsafe {
+            type FspMemoryInit = unsafe extern "win64" fn(
+                FspmUpdDataPtr: *mut core::ffi::c_void,
+                HobListPtr: *mut *mut core::ffi::c_void,
+            ) -> efi::EFI_STATUS;
+            let fspm = core::mem::transmute::<usize, FspMemoryInit>(fsp_base + fspm_entry);
+            fspm(core::mem::transmute(&mut fspm_upd), 0 as *mut _)
+        };
+        write!(w, "Returned {}\r\n", status).unwrap();
+    } else {
+        write!(w, "Could not find FspMemoryInit\r\n").unwrap();
+    }
+
     // TODO: Get these values from the fdt
     let payload = &mut BzImage {
         low_mem_size: 0x80_000_000,
@@ -46,6 +114,61 @@ pub extern "C" fn _start(_fdt_address: usize) -> ! {
 
     write!(w, "Unexpected return from payload\r\n").unwrap();
     arch::halt()
+}
+
+const FSP_FFS_INFORMATION_FILE_GUID: GUID = GUID(
+    0x912740be,
+    0x2284,
+    0x4734,
+    [0xb9, 0x71, 0x84, 0xb0, 0x27, 0x35, 0x3f, 0x0c],
+);
+const FSP_S_UPD_FFS_GUID: GUID = GUID(
+    0xe3cd9b18,
+    0x998c,
+    0x4f76,
+    [0xb6, 0x5e, 0x98, 0xb1, 0x54, 0xe5, 0x44, 0x6f],
+);
+const FILE_TYPES: &'static [u32] = &[efi::EFI_FV_FILETYPE_RAW];
+
+#[derive(Debug)]
+struct FspInfoEntry {
+    addr: usize,
+    info: efi::FSP_INFO_HEADER,
+}
+type FspInfos = Vec<FspInfoEntry, U4>;
+
+fn fsp_memory_init_entry(infos: &FspInfos) -> Option<usize> {
+    for entry in infos.iter() {
+        if entry.info.ComponentAttribute & 0xf000 == 0x2000 {
+            return Some(entry.addr + entry.info.FspMemoryInitEntryOffset as usize);
+        }
+    }
+    None
+}
+
+#[no_mangle]
+fn find_fsp(fspfv: &[u8]) -> Result<FspInfos, uefi::FvTraverseError> {
+    let mut infos = FspInfos::new();
+
+    fv_traverse(fspfv, FILE_TYPES, |sec_info, sec_data: SectionData| {
+        // All three parts must match.
+        match (sec_info.ffs_guid, sec_info.ffs_type, sec_info.sec_type) {
+            (FSP_FFS_INFORMATION_FILE_GUID, efi::EFI_FV_FILETYPE_RAW, efi::EFI_SECTION_RAW) => {
+                if infos.len() != infos.capacity() {
+                    infos
+                        .push(FspInfoEntry {
+                            addr: sec_info.fv_base,
+                            info: unsafe { *(sec_data.as_ptr() as *const efi::FSP_INFO_HEADER) }
+                                .clone(),
+                        })
+                        .unwrap();
+                }
+            }
+            (FSP_S_UPD_FFS_GUID, efi::EFI_FV_FILETYPE_RAW, efi::EFI_SECTION_RAW) => (),
+            _ => (),
+        }
+    })?;
+    Ok(infos)
 }
 
 #[panic_handler]
