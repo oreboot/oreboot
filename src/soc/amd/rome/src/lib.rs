9cebe64ff70922998508bbebd4949952f6078e95
Ronald G. Minnich <rminnich@gmail.com>
2021-02-25 08:29:59 -0800

    soc/amd/rome: Create SOC files
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/soc/amd/rome/src/lib.rs
+++ src/soc/amd/rome/src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(llvm_asm)]
 #![no_std]
 #![deny(warnings)]
 
@@ -34,3 +35,38 @@ pub fn soc_init(w: &mut impl core::fmt::Write) -> Result<(), &'static str> {
     }
     Ok(())
 }
+#![no_std]
+#![no_main]
+#![feature(global_asm)]
+
+/// Write 32 bits to port
+unsafe fn outl(port: u16, val: u32) {
+    llvm_asm!("outl %eax, %dx" :: "{dx}"(port), "{al}"(val));
+}
+
+/// Read 32 bits from port
+unsafe fn inl(port: u16) -> u32 {
+    let ret: u32;
+    llvm_asm!("inl %dx, %eax" : "={ax}"(ret) : "{dx}"(port) :: "volatile");
+    return ret;
+}
+
+pub fn smn_read(a: u32) -> u32 {
+    // the smn device is at (0)
+    unsafe {
+        outl(0xcf8, 0x8000_00b8);
+        outl(0xcfc, a);
+        outl(0xcf8, 0x8000_00bc);
+        inl(0xcfc)
+    }
+}
+
+pub fn smn_write(a: u32, v: u32) {
+    unsafe {
+        outl(0xcf8, 0x800000b8);
+        outl(0xcfc, a);
+        outl(0xcf8, 0x800000bc);
+        outl(0xcfc, v);
+    }
+}
+
