06d5de85f142f5f2ea158b87d5f5e2bbd97273b2
Danny Milosavljevic <danny.milo@datacom.wien>
2021-03-03 22:44:30 +0100

    pci: Add some parameter value checking.
    Signed-off-by: Danny Milosavljevic <danny.milo@datacom.wien>

--- src/soc/amd/common/pci/src/lib.rs
+++ src/soc/amd/common/pci/src/lib.rs
@@ -1,3 +1,4 @@
 #![no_std]
+#![feature(const_panic)]
 
 pub mod pci;
--- src/soc/amd/common/pci/src/pci.rs
+++ src/soc/amd/common/pci/src/pci.rs
@@ -13,18 +13,10 @@ pub struct PciAddress {
 impl PciAddress {
     #[inline(always)]
     pub const fn mmio_config_address(self) -> u32 {
-        /*if self.segment >= 16 {
-            panic!("PciAddress segment is out of range: {}", self.segment);
-        }
-        if self.device >= 32 {
-            panic!("PciAddress device is out of range: {}", self.device);
-        }
-        if self.function >= 8 {
-            panic!("PciAddress function is out of range: {}", self.function);
-        }
-        if self.offset >= 4096 {
-            panic!("PciAddress offset is out of range: {}", self.offset);
-        }*/
+        assert!(self.segment < 16);
+        assert!(self.device < 32);
+        assert!(self.function < 8);
+        assert!(self.offset < 4096);
         MMIO_CFG_BASE | ((self.segment as u32) << 28) | ((self.bus as u32) << 20) | ((self.device as u32) << 15) | ((self.function as u32) << 12) | (self.offset as u32)
     }
 }
