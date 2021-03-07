d8c476967dc6c6d9f391424b71d06fc0ce6e1f09
Danny Milosavljevic <danny.milo@datacom.wien>
2021-03-07 21:35:42 +0100

    Picasso: Rename "SMU" struct to "MP1", "SmuServiceRequest" to just "ServiceRequest".
    Signed-off-by: Danny Milosavljevic <danny.milo@datacom.wien>

--- src/soc/amd/picasso/src/lib.rs
+++ src/soc/amd/picasso/src/lib.rs
@@ -4,29 +4,29 @@
 pub mod mp1;
 
 pub fn soc_init(w: &mut impl core::fmt::Write) -> Result<(), &'static str> {
-    let smu = smu::SMU::new();
-    match smu.test(42) {
+    let mp1 = mp1::MP1::new();
+    match mp1.test(42) {
         Ok(v) => {
-            write!(w, "SMU test(42) result: {}\r\n", v).unwrap();
+            write!(w, "MP1 test(42) result: {}\r\n", v).unwrap();
         }
         Err(e) => {
-            write!(w, "SMU test(42) error: {}\r\n", e).unwrap();
+            write!(w, "MP1 test(42) error: {}\r\n", e).unwrap();
         }
     }
-    match smu.smu_version() {
+    match mp1.smu_version() {
         Ok((major, minor)) => {
-            write!(w, "SMU smu version result: {}.{}\r\n", major, minor).unwrap();
+            write!(w, "MP1 smu version result: {}.{}\r\n", major, minor).unwrap();
         }
         Err(e) => {
-            write!(w, "SMU smu version error: {}\r\n", e).unwrap();
+            write!(w, "MP1 smu version error: {}\r\n", e).unwrap();
         }
     }
-    match smu.interface_version() {
+    match mp1.interface_version() {
         Ok((major, minor)) => {
-            write!(w, "SMU interface version result: {}.{}\r\n", major, minor).unwrap();
+            write!(w, "MP1 interface version result: {}.{}\r\n", major, minor).unwrap();
         }
         Err(e) => {
-            write!(w, "SMU interface version error: {}\r\n", e).unwrap();
+            write!(w, "MP1 interface version error: {}\r\n", e).unwrap();
         }
     }
     Ok(())
--- src/soc/amd/picasso/src/mp1.rs
+++ src/soc/amd/picasso/src/mp1.rs
@@ -7,72 +7,72 @@ use vcell::VolatileCell;
 
 // See coreboot:src/soc/amd/common/block/smu/smu.c
 
-const SMU_MESSAGE_ID_SMN: u32 = 0x3B1_0528;
-const SMU_MESSAGE_RESPONSE_SMN: u32 = 0x3B1_0564;
-const SMU_MESSAGE_ARGUMENT_0_SMN: u32 = 0x3B1_0998;
-const SMU_MESSAGE_ARGUMENT_1_SMN: u32 = 0x3B1_099C;
-const SMU_MESSAGE_ARGUMENT_2_SMN: u32 = 0x3B1_09A0;
-const SMU_MESSAGE_ARGUMENT_3_SMN: u32 = 0x3B1_09A4;
-const SMU_MESSAGE_ARGUMENT_4_SMN: u32 = 0x3B1_09A8;
-const SMU_MESSAGE_ARGUMENT_5_SMN: u32 = 0x3B1_09AC;
+const MESSAGE_ID_SMN: u32 = 0x3B1_0528;
+const MESSAGE_RESPONSE_SMN: u32 = 0x3B1_0564;
+const MESSAGE_ARGUMENT_0_SMN: u32 = 0x3B1_0998;
+const MESSAGE_ARGUMENT_1_SMN: u32 = 0x3B1_099C;
+const MESSAGE_ARGUMENT_2_SMN: u32 = 0x3B1_09A0;
+const MESSAGE_ARGUMENT_3_SMN: u32 = 0x3B1_09A4;
+const MESSAGE_ARGUMENT_4_SMN: u32 = 0x3B1_09A8;
+const MESSAGE_ARGUMENT_5_SMN: u32 = 0x3B1_09AC;
 
-pub struct SmuServiceRequest {
+struct ServiceRequest {
     command: u32,
     data: [u32; 6],
 }
 
 #[derive(Debug)]
-pub struct SmuServiceResponse {
+struct ServiceResponse {
     status: u32,
     data: [u32; 6],
 }
 
-pub struct SMU<'a> {
+pub struct MP1<'a> {
     NB_SMN_INDEX_2: &'a VolatileCell<u32>,
     NB_SMN_DATA_2: &'a VolatileCell<u32>,
 }
 
-impl SMU<'_> {
+impl MP1<'_> {
     pub fn new() -> Self {
-        SMU { NB_SMN_INDEX_2: config32(PciAddress { segment: 0, bus: 0, device: 0, function: 0, offset: 0xb8 }), NB_SMN_DATA_2: config32(PciAddress { segment: 0, bus: 0, device: 0, function: 0, offset: 0xbc }) }
+        Self { NB_SMN_INDEX_2: config32(PciAddress { segment: 0, bus: 0, device: 0, function: 0, offset: 0xb8 }), NB_SMN_DATA_2: config32(PciAddress { segment: 0, bus: 0, device: 0, function: 0, offset: 0xbc }) }
     }
 
-    fn smu_register_read(&self, a: u32) -> u32 {
+    fn register_read_smn(&self, a: u32) -> u32 {
         self.NB_SMN_INDEX_2.set(a);
         self.NB_SMN_DATA_2.get()
     }
 
-    fn smu_register_write(&self, a: u32, v: u32) {
+    fn register_write_smn(&self, a: u32, v: u32) {
         self.NB_SMN_INDEX_2.set(a);
         self.NB_SMN_DATA_2.set(v)
     }
 
-    fn service_call(&self, request: SmuServiceRequest) -> Result<SmuServiceResponse, u32> {
-        //let mut response: u32 = self.smu_register_read(SMU_MESSAGE_RESPONSE_SMN);
-        self.smu_register_write(SMU_MESSAGE_RESPONSE_SMN, 0);
-        self.smu_register_write(SMU_MESSAGE_ARGUMENT_0_SMN, request.data[0]);
-        self.smu_register_write(SMU_MESSAGE_ARGUMENT_1_SMN, request.data[1]);
-        self.smu_register_write(SMU_MESSAGE_ARGUMENT_2_SMN, request.data[2]);
-        self.smu_register_write(SMU_MESSAGE_ARGUMENT_3_SMN, request.data[3]);
-        self.smu_register_write(SMU_MESSAGE_ARGUMENT_4_SMN, request.data[4]);
-        self.smu_register_write(SMU_MESSAGE_ARGUMENT_5_SMN, request.data[5]);
-        self.smu_register_write(SMU_MESSAGE_ID_SMN, request.command);
-        let mut response: u32 = self.smu_register_read(SMU_MESSAGE_RESPONSE_SMN);
+    fn service_call(&self, request: ServiceRequest) -> Result<ServiceResponse, u32> {
+        //let mut response: u32 = self.register_read_smn(MESSAGE_RESPONSE_SMN);
+        self.register_write_smn(MESSAGE_RESPONSE_SMN, 0);
+        self.register_write_smn(MESSAGE_ARGUMENT_0_SMN, request.data[0]);
+        self.register_write_smn(MESSAGE_ARGUMENT_1_SMN, request.data[1]);
+        self.register_write_smn(MESSAGE_ARGUMENT_2_SMN, request.data[2]);
+        self.register_write_smn(MESSAGE_ARGUMENT_3_SMN, request.data[3]);
+        self.register_write_smn(MESSAGE_ARGUMENT_4_SMN, request.data[4]);
+        self.register_write_smn(MESSAGE_ARGUMENT_5_SMN, request.data[5]);
+        self.register_write_smn(MESSAGE_ID_SMN, request.command);
+        let mut response: u32 = self.register_read_smn(MESSAGE_RESPONSE_SMN);
         while response == 0 {
-            response = self.smu_register_read(SMU_MESSAGE_RESPONSE_SMN);
+            response = self.register_read_smn(MESSAGE_RESPONSE_SMN);
         }
 
         if response == 1 {
             // OK
-            Ok(SmuServiceResponse {
+            Ok(ServiceResponse {
                 status: response,
                 data: [
-                    self.smu_register_read(SMU_MESSAGE_ARGUMENT_0_SMN),
-                    self.smu_register_read(SMU_MESSAGE_ARGUMENT_1_SMN),
-                    self.smu_register_read(SMU_MESSAGE_ARGUMENT_2_SMN),
-                    self.smu_register_read(SMU_MESSAGE_ARGUMENT_3_SMN),
-                    self.smu_register_read(SMU_MESSAGE_ARGUMENT_4_SMN),
-                    self.smu_register_read(SMU_MESSAGE_ARGUMENT_5_SMN),
+                    self.register_read_smn(MESSAGE_ARGUMENT_0_SMN),
+                    self.register_read_smn(MESSAGE_ARGUMENT_1_SMN),
+                    self.register_read_smn(MESSAGE_ARGUMENT_2_SMN),
+                    self.register_read_smn(MESSAGE_ARGUMENT_3_SMN),
+                    self.register_read_smn(MESSAGE_ARGUMENT_4_SMN),
+                    self.register_read_smn(MESSAGE_ARGUMENT_5_SMN),
                 ],
             })
         } else {
@@ -81,22 +81,22 @@ impl SMU<'_> {
     }
 
     pub fn test(&self, v: u32) -> Result<u32, u32> {
-        let result = self.service_call(SmuServiceRequest { command: 1, data: [v, 0, 0, 0, 0, 0] })?;
+        let result = self.service_call(ServiceRequest { command: 1, data: [v, 0, 0, 0, 0, 0] })?;
         Ok(result.data[0])
     }
 
     pub fn smu_version(&self) -> Result<(u32, u32), u32> {
-        let result = self.service_call(SmuServiceRequest { command: 2, data: [0; 6] })?;
+        let result = self.service_call(ServiceRequest { command: 2, data: [0; 6] })?;
         Ok((result.data[0], result.data[1]))
     }
 
     pub fn interface_version(&self) -> Result<(u32, u32), u32> {
-        let result = self.service_call(SmuServiceRequest { command: 3, data: [0; 6] })?;
+        let result = self.service_call(ServiceRequest { command: 3, data: [0; 6] })?;
         Ok((result.data[0], result.data[1]))
     }
 }
 
-impl Default for SMU<'_> {
+impl Default for MP1<'_> {
     fn default() -> Self {
         Self::new()
     }
