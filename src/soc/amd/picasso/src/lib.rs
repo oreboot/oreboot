e4c161137bae8230b2cd0f1e85050a8baa192bfd
Daniel Maslowski <info@orangecms.org>
2021-03-10 03:04:54 +0100

    soc/picasso: df topology
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/soc/amd/picasso/src/lib.rs
+++ src/soc/amd/picasso/src/lib.rs
@@ -21,5 +21,12 @@ pub fn soc_init(w: &mut impl core::fmt::Write) -> Result<(), &'static str> {
             write!(w, "MP1 smu version error: {:x?}\r\n", e).unwrap();
         }
     }
+
+    let topology = df::FabricTopology::new();
+    // {:#x?} doesn't work correctly because it only prints LF
+    // for newline, no CR.
+    // write!(w, "Topology: {:x?}\r\n", topology).unwrap();
+    //write!(w, "PIEs: {:?}\r\n", topology.pie_count);
+    //write!(w, "IOMS: {:?}\r\n", topology.ioms_count);
     Ok(())
 }
