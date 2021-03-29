8e095a41345de70d2571ed52e6cfc67125f825d9
Danny Milosavljevic <danny.milo@datacom.wien>
2021-03-29 14:51:13 +0200

    MPMailbox: Add Result and MPMailboxError type.
    Signed-off-by: Danny Milosavljevic <danny.milo@datacom.wien>

--- src/soc/amd/picasso/src/lib.rs
+++ src/soc/amd/picasso/src/lib.rs
@@ -6,18 +6,18 @@ pub fn soc_init(w: &mut impl core::fmt::Write) -> Result<(), &'static str> {
     let mp1 = MPMailbox::<6>::new(0x3B1_0528, 0x3B1_0564, 0x3B1_0998);
     match mp1.test(42) {
         Ok(v) => {
-            write!(w, "MP1 test(42) result: {}\r\n", v).unwrap();
+            write!(w, "MP1 test(42) result: {:x?}\r\n", v).unwrap();
         }
         Err(e) => {
-            write!(w, "MP1 test(42) error: {}\r\n", e).unwrap();
+            write!(w, "MP1 test(42) error: {:x?}\r\n", e).unwrap();
         }
     }
     match mp1.smu_version() {
         Ok(version) => {
-            write!(w, "MP1 smu version result: {:#x?}\r\n", version).unwrap();
+            write!(w, "MP1 smu version result: {:x?}\r\n", version).unwrap();
         }
         Err(e) => {
-            write!(w, "MP1 smu version error: {}\r\n", e).unwrap();
+            write!(w, "MP1 smu version error: {:x?}\r\n", e).unwrap();
         }
     }
     Ok(())
