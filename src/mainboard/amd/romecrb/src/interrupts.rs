fd3ba7f0fa38f741a95985774a717f91a9fa7cbe
Danny Milosavljevic <danny.milo@datacom.wien>
2021-04-15 21:43:01 +0200

    Rome: Try avoiding lazy_static for now.
    Signed-off-by: Danny Milosavljevic <danny.milo@datacom.wien>

--- src/mainboard/amd/romecrb/src/interrupts.rs
+++ src/mainboard/amd/romecrb/src/interrupts.rs
@@ -1,8 +1,8 @@
 #![feature(llvm_asm)]
 
+use lazy_static::lazy_static;
 use x86_64::structures::idt::InterruptDescriptorTable;
 use x86_64::structures::idt::InterruptStackFrame;
-use lazy_static::lazy_static;
 
 unsafe fn outl(port: u16, val: u32) {
     llvm_asm!("outl %eax, %dx" :: "{dx}"(port), "{al}"(val));
@@ -19,15 +19,27 @@ extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut InterruptStackF
     panic!("Exception: Double fault.\r\n{:#?}", stack_frame);
 }
 
-lazy_static! {
-    static ref IDT: InterruptDescriptorTable = {
-        let mut idt = InterruptDescriptorTable::new();
-        idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.double_fault.set_handler_fn(double_fault_handler);
-        idt
-    };
+extern "x86-interrupt" fn divide_error_handler(stack_frame: &mut InterruptStackFrame) {
+    panic!("Exception: Division by zero.\r\n{:#?}", stack_frame);
 }
 
+//lazy_static! {
+//    static ref IDT: InterruptDescriptorTable = {
+//        let mut idt = InterruptDescriptorTable::new();
+//        idt.breakpoint.set_handler_fn(breakpoint_handler);
+//        idt.double_fault.set_handler_fn(double_fault_handler);
+//        idt.divide_error.set_handler_fn(divide_error_handler);
+//        idt
+//    };
+//}
+
 pub fn init_idt() {
-    IDT.load();
+    //    IDT.load();
+    unsafe {
+        let mut idt = 0x100000 as *mut InterruptDescriptorTable;
+        (*idt).breakpoint.set_handler_fn(breakpoint_handler);
+        (*idt).double_fault.set_handler_fn(double_fault_handler);
+        (*idt).divide_error.set_handler_fn(divide_error_handler);
+        (*idt).load();
+    }
 }
