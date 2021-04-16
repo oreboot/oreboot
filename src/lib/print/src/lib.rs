2ea796dd28d6673fa3e77b2ca19ac2635e68a91b
Danny Milosavljevic <danny.milo@datacom.wien>
2021-04-17 01:02:13 +0200

    print: Make WriteTo use a dyn Driver instead of determining Driver to use via the generic argument at compile time.
    Signed-off-by: Danny Milosavljevic <danny.milo@datacom.wien>

--- src/lib/print/src/lib.rs
+++ src/lib/print/src/lib.rs
@@ -3,17 +3,17 @@
 use core::fmt;
 use model::Driver;
 
-pub struct WriteTo<'a, D: Driver> {
-    drv: &'a mut D,
+pub struct WriteTo<'a> {
+    drv: &'a mut dyn Driver,
 }
 
-impl<'a, D: Driver> WriteTo<'a, D> {
-    pub fn new(drv: &'a mut D) -> Self {
+impl<'a> WriteTo<'a> {
+    pub fn new(drv: &'a mut dyn Driver) -> Self {
         WriteTo { drv }
     }
 }
 
-impl<'a, D: Driver> fmt::Write for WriteTo<'a, D> {
+impl<'a> fmt::Write for WriteTo<'a> {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         match self.drv.pwrite(s.as_bytes(), 0) {
             Err(_) => Err(fmt::Error),
