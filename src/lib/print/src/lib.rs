f301322ca0c23049c1861c58560de1166eb06197
Danny Milosavljevic <danny.milo@datacom.wien>
2021-04-17 01:25:56 +0200

    Reinstate original WriteTo; introduce extra WriteToDyn and use it.
    Signed-off-by: Danny Milosavljevic <danny.milo@datacom.wien>

--- src/lib/print/src/lib.rs
+++ src/lib/print/src/lib.rs
@@ -3,17 +3,38 @@
 use core::fmt;
 use model::Driver;
 
-pub struct WriteTo<'a> {
-    drv: &'a mut dyn Driver,
+pub struct WriteTo<'a, D: Driver> {
+    drv: &'a mut D,
 }
 
-impl<'a> WriteTo<'a> {
-    pub fn new(drv: &'a mut dyn Driver) -> Self {
-        WriteTo { drv }
+impl<'a, D: Driver> WriteTo<'a, D> {
+    pub fn new(drv: &'a mut D) -> Self {
+        Self { drv }
     }
 }
 
-impl<'a> fmt::Write for WriteTo<'a> {
+impl<'a, D: Driver> fmt::Write for WriteTo<'a, D> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        match self.drv.pwrite(s.as_bytes(), 0) {
+            Err(_) => Err(fmt::Error),
+            _ => Ok(()),
+        }
+    }
+}
+
+//
+
+pub struct WriteToDyn<'a> {
+    drv: &'a mut dyn Driver,
+}
+
+impl<'a> WriteToDyn<'a> {
+    pub fn new(drv: &'a mut dyn Driver) -> Self {
+        Self { drv }
+    }
+}
+
+impl<'a> fmt::Write for WriteToDyn<'a> {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         match self.drv.pwrite(s.as_bytes(), 0) {
             Err(_) => Err(fmt::Error),
--- src/mainboard/amd/romecrb/src/mainboard.rs
+++ src/mainboard/amd/romecrb/src/mainboard.rs
@@ -202,7 +202,7 @@ impl Driver for MainBoard {
             //post.pwrite(&p, 0x80).unwrap();
 
             let r: &mut dyn Driver = self.text_outputs()[0];
-            let w = &mut print::WriteTo::new(r);
+            let w = &mut print::WriteToDyn::new(r);
 
             // Logging.
             smnhack(w, 0x13B1_02F4, 0x00000000u32);
