81dc0a29e0760a967496e288c6335a0d0af25f19
Danny Milosavljevic <danny.milo@datacom.wien>
2021-04-17 03:09:43 +0200

    print: Introduce WriteToDyn.
    Signed-off-by: Danny Milosavljevic <danny.milo@datacom.wien>

--- src/lib/print/src/lib.rs
+++ src/lib/print/src/lib.rs
@@ -9,11 +9,32 @@ pub struct WriteTo<'a, D: Driver> {
 
 impl<'a, D: Driver> WriteTo<'a, D> {
     pub fn new(drv: &'a mut D) -> Self {
-        WriteTo { drv }
+        Self { drv }
     }
 }
 
 impl<'a, D: Driver> fmt::Write for WriteTo<'a, D> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        match self.drv.pwrite(s.as_bytes(), 0) {
+            Err(_) => Err(fmt::Error),
+            _ => Ok(()),
+        }
+    }
+}
+
+//
+
+pub struct WriteToDyn<'a> {
+    drv: &'a mut dyn Driver,
+}
+
+impl<'a> WriteToDyn<'a> {
+    pub fn new(drv: &'a mut dyn Driver) -> Self {
+        Self { drv }
+    }
+}
+
+impl<'a> fmt::Write for WriteToDyn<'a> {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         match self.drv.pwrite(s.as_bytes(), 0) {
             Err(_) => Err(fmt::Error),
