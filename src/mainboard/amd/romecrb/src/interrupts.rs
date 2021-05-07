27aea9bb3f8feae311d508a60b69cf324c953ea6
Danny Milosavljevic <danny.milo@datacom.wien>
2021-05-07 20:23:04 +0200

    AMD Rome: Make exceptions work--and test it with a "divide error" exception.
    Signed-off-by: Danny Milosavljevic <danny.milo@datacom.wien>

--- src/mainboard/amd/romecrb/src/interrupts.rs
+++ src/mainboard/amd/romecrb/src/interrupts.rs
@@ -1,8 +1,8 @@
-#![feature(llvm_asm)]
-
 use lazy_static::lazy_static;
+use x86_64::structures::idt::EntryOptions;
 use x86_64::structures::idt::InterruptDescriptorTable;
 use x86_64::structures::idt::InterruptStackFrame;
+use x86_64::PrivilegeLevel;
 
 fn outb(port: u16, val: u8) {
     unsafe {
@@ -10,45 +10,59 @@ fn outb(port: u16, val: u8) {
     }
 }
 
-extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame) {
-    unsafe {
-        outb(0x80, 0x11);
-    }
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     panic!("Exception: Breakpoint.\r\n{:#?}", stack_frame);
 }
 
-extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut InterruptStackFrame, _error_code: u64) -> ! {
+extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
     panic!("Exception: Double fault.\r\n{:#?}", stack_frame);
+    arch::halt();
 }
 
-extern "x86-interrupt" fn divide_error_handler(stack_frame: &mut InterruptStackFrame) {
+extern "x86-interrupt" fn general_protection_fault_handler(stack_frame: InterruptStackFrame, _error_code: u64) {
+    panic!("Exception: General protection fault.\r\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn divide_error_handler(stack_frame: InterruptStackFrame) {
     panic!("Exception: Division by zero.\r\n{:#?}", stack_frame);
 }
 
-extern "x86-interrupt" fn interrupt_handler(stack_frame: &mut InterruptStackFrame) {
+extern "x86-interrupt" fn interrupt_handler(stack_frame: InterruptStackFrame) {
     panic!("Interrupt.\r\n{:#?}", stack_frame);
 }
+
 
 //lazy_static! {
 //    static ref IDT: InterruptDescriptorTable = {
 //        let mut idt = InterruptDescriptorTable::new();
 //        idt.breakpoint.set_handler_fn(breakpoint_handler);
 //        idt.double_fault.set_handler_fn(double_fault_handler);
+//        idt.general_protection_fault.set_handler_fn(general_protection_fault_handler);
 //        idt.divide_error.set_handler_fn(divide_error_handler);
+//        idt[32].set_handler_fn(interrupt_handler);
 //        idt
 //    };
 //}
 
 pub fn init_idt() {
-    //    IDT.load();
+//    IDT.load();
+
     unsafe {
         let mut idt = 0x100000 as *mut InterruptDescriptorTable;
-        (*idt).breakpoint.set_handler_fn(breakpoint_handler);
-        (*idt).double_fault.set_handler_fn(double_fault_handler);
-        (*idt).divide_error.set_handler_fn(divide_error_handler);
-        (*idt)[32].set_handler_fn(interrupt_handler);
+        (*idt) = InterruptDescriptorTable::new();
+        (*idt).breakpoint.set_handler_fn(breakpoint_handler).set_privilege_level(PrivilegeLevel::Ring0);
+        (*idt).double_fault.set_handler_fn(double_fault_handler).set_privilege_level(PrivilegeLevel::Ring0);
+        (*idt).general_protection_fault.set_handler_fn(general_protection_fault_handler).set_privilege_level(PrivilegeLevel::Ring0);
+        (*idt).divide_error.set_handler_fn(divide_error_handler).set_privilege_level(PrivilegeLevel::Ring0);
+        //(*idt)[32].set_handler_fn(interrupt_handler);
+        unsafe { llvm_asm!("nop" :::: "volatile") }
         (*idt).load();
+        unsafe { llvm_asm!("nop" :::: "volatile") }
     }
+ unsafe {
+            llvm_asm!("xorl %ebx, %ebx\ndiv %ebx" : /* no outputs */ : /* no inputs */ : "ebx" : "volatile");
+        }
+    panic!("X");
 }
 
 pub fn init_pics() {
