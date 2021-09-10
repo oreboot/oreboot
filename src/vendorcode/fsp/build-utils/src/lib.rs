4c1c802a67bb581c8f78ae140cf45681461dd6dd
Jordan <jhand@google.com>
2021-09-10 13:48:32 -0700

    Refactor FSP code to build for different architectures (#471)
    Split up FSP and UEFI code so that non arch-dependant code can be put in
    the uefi and fsp-common crates and architecture specific code can be put
    in fsp-qemu32-sys and fsp-qemu-sys for Ia32 and X64 respectively.
    
    Note that FSP is 32-bit for all platforms except the x64 QEMU build.
    
    Signed-off-by: Jordan Hand <jhand@google.com>

---
+++ src/vendorcode/fsp/build-utils/src/lib.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+use std::process::{exit, Command};
+
+pub enum FspArchitecture {
+    X64,
+    Ia32,
+}
+
+pub fn build_qemu_fsp(oreboot_root: &str, arch: FspArchitecture) -> std::io::Result<()> {
+    let arch_name = match arch {
+        FspArchitecture::X64 => "x64",
+        FspArchitecture::Ia32 => "ia32",
+    };
+
+    let root_path = PathBuf::from(oreboot_root);
+
+    // Build FSP binaries and generate header files.
+    let status = Command::new("python3")
+        .args(&["BuildFsp.py", "build", "-p", "qemu", "-a", &arch_name])
+        .current_dir(root_path.join("3rdparty/fspsdk"))
+        .status()
+        .expect("failed to build FSP");
+    if !status.success() {
+        println!("Failed to build FSP");
+        exit(1);
+    }
+
+    // Copy FSP binaries to output path.
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let edk2_fv_dir = root_path.join("3rdparty/fspsdk/Build/QemuFspPkg/DEBUG_GCC5/FV");
+    fs::copy(edk2_fv_dir.join("QEMUFSP.fd"), out_dir.join("QEMUFSP.fd"))?;
+
+    Ok(())
+}
