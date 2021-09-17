6f92df75904fc52d396d919e3316df52514f5ae2
Lean Sheng Tan <tanleansheng@outlook.com>
2021-09-18 03:41:26 +0800

    upxtreme: Initial commit
    Signed-off-by: Lean Sheng Tan <tanleansheng@outlook.com>

--- src/vendorcode/fsp/coffeelake/src/lib.rs
+++ src/vendorcode/fsp/coffeelake/src/lib.rs
@@ -1,12 +1,12 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![no_std]
 
-// Types from the ffi crate are used instead of the generated ones.
-#[allow(unused_imports)]
-use efi::ffi::{
-    BOOLEAN, CHAR16, CHAR8, EFI_GUID, EFI_GUID as GUID, INT16, INT32, INT64, INT8, UINT16, UINT32,
-    UINT64, UINT8, UINTN,
-};
+// Rust types are used instead of generated ones.
+#[repr(C)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct EFI_GUID(pub UINT32, pub UINT16, pub UINT16, pub [UINT8; 8]);
+pub type GUID = EFI_GUID;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
