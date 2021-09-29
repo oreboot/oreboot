a42a0b3cc852152f4194862dcfc939d5d52af655
Jordan Hand <jhand@google.com>
2021-09-29 02:36:32 +0000

    Fix the build for upxtreme
    Get the upxtreme board building. It almost certainly won't boot because
    FSP UPD parameters are incomplete. But this is a start.
    
    Signed-off-by: Jordan Hand <jhand@google.com>

--- src/mainboard/aaeon/upxtreme/src/main.rs
+++ src/mainboard/aaeon/upxtreme/src/main.rs
@@ -2,19 +2,24 @@
 #![no_std]
 #![no_main]
 #![feature(global_asm)]
+// Clippy stupidly suggests using functions from std even you we have declared
+// no_std. Shame on clippy.
+#![allow(clippy::zero_ptr)]
 
 use arch::bzimage::BzImage;
 use arch::ioport::IOPort;
 use core::fmt::Write;
 use core::panic::PanicInfo;
 use model::Driver;
-use print;
 use uart::debug_port::DebugPort;
 use uart::i8250::I8250;
 
 use rpp_procedural::preprocess_asm;
 
-use fsp_cfl_sys as fsp64;
+use fsp_cfl_sys as fsp32;
+
+// TODO: it might be nice to do some trickery where fsp_*_sys just exposes
+// everything in fsp_common.
 use fsp_common as fsp;
 
 // Unless we mention the fsp_cfl_sys crate, the compiler will optimize it away. This crate
@@ -25,80 +30,85 @@ global_asm!(preprocess_asm!(
     "../../../arch/x86/x86_64/src/bootblock_nomem.S"
 ));
 
-fn call_fspm(fsp_base: usize, fspm_entry: usize) -> fsp64::EFI_STATUS {
+fn call_fspm(fsp_base: usize, fspm_entry: usize) -> u32 {
+    let mut fspm_cfg = fsp32::FSP_M_CONFIG::default();
+    let mut fspm_test_cfg = fsp32::FSP_M_TEST_CONFIG::default();
+
+    // These values are taken from
+    // https://github.com/coreboot/coreboot/blob/master/src/soc/intel/cannonlake/romstage/fsp_params.c#L20
+    // TODO: Fill out the remaining parameters
+    // TODO: Use constant defines for these instead of magic numbers. For example, define TsegSize
+    // as CONFIG_SMM_TSEG_SIZE. This can be defined in fsp_cfl_sys.
+    fspm_cfg.InternalGfx = 0;
+    fspm_cfg.IgdDvmt50PreAlloc = 0;
+    fspm_cfg.TsegSize = 0x800000;
+    fspm_cfg.IedSize = 0x400000;
+    fspm_cfg.SaGv = 0; // Allows memory training to happen at different frequencies? Disabled for now
+
+    fspm_test_cfg.PanelPowerEnable = 0;
+
     // TODO: This struct has to be aligned to 4.
     // mut because we can't make the assumption FSP won't modify it.
-    let mut fspm_upd = fsp64::FSPM_UPD {
-        FspUpdHeader: fsp64::FSP_UPD_HEADER {
-            Signature: fsp64::FSPM_UPD_SIGNATURE,
+    let mut fspm_upd = fsp32::FSPM_UPD {
+        FspUpdHeader: fsp32::FSP_UPD_HEADER {
+            Signature: fsp32::FSPM_UPD_SIGNATURE,
             Revision: 2, // FSP 2.2
             Reserved: [0u8; 23],
         },
-        FspmArchUpd: fsp64::FSPM_ARCH_UPD {
+        FspmArchUpd: fsp32::FSPM_ARCH_UPD {
             Revision: 2, // FSP 2.2
             Reserved: [0u8; 3],
-            NvsBufferPtr: 0,        // non-volatile storage not available
-            StackBase: 0x20000000,  // TODO: I picked this at random
-            StackSize: 0x10000,     // TODO: I picked this at random
+            NvsBufferPtr: 0 as *mut core::ffi::c_void, // non-volatile storage not available
+            StackBase: 0x20000000 as *mut core::ffi::c_void, // TODO: I picked this at random
+            StackSize: 0x10000,                        // TODO: I picked this at random
             BootLoaderTolumSize: 0, // Don't reserve "top of low usable memory" for bootloader.
-            BootMode: fsp64::BOOT_WITH_FULL_CONFIGURATION,
-            FspEventHandler: 0 as *mut fsp64::FSP_EVENT_HANDLER, // optional
-            Reserved1: [0u8; 4],
+            BootMode: fsp32::BOOT_WITH_FULL_CONFIGURATION,
+            Reserved1: [0u8; 8],
         },
-        FspmConfig: fsp64::FSP_M_CONFIG {
-            SerialDebugPortAddress: 0x3f8,
-            SerialDebugPortType: 1,       // I/O
-            SerialDebugPortDevice: 3,     // External Device
-            SerialDebugPortStrideSize: 0, // 1
-            UnusedUpdSpace0: [0; 49],
-            ReservedFspmUpd: [0; 4],
-        },
-        UnusedUpdSpace1: [0u8; 2],
+        FspmConfig: fspm_cfg,
+        FspmTestConfig: fspm_test_cfg,
+        UnusedUpdSpace6: 0u8,
         UpdTerminator: 0x55AA, // ???
     };
 
-    let status = unsafe {
+    unsafe {
         type FspMemoryInit = unsafe extern "win64" fn(
             FspmUpdDataPtr: *mut core::ffi::c_void,
             HobListPtr: *mut *mut core::ffi::c_void,
-        ) -> fsp64::EFI_STATUS;
+        ) -> u32;
         let fspm = core::mem::transmute::<usize, FspMemoryInit>(fsp_base + fspm_entry);
         fspm(core::mem::transmute(&mut fspm_upd), 0 as *mut _)
-    };
-
-    status
+    }
 }
 
-fn call_fsps(fsp_base: usize, fsps_entry: usize) -> fsp64::EFI_STATUS {
+fn call_fsps(fsp_base: usize, fsps_entry: usize) -> u32 {
+    let mut fsps_config = fsp32::FSP_S_CONFIG::default();
+    let fsps_test_config = fsp32::FSP_S_TEST_CONFIG::default();
+
+    fsps_config.LogoSize = 0;
+    fsps_config.LogoPtr = 0;
+    fsps_config.GraphicsConfigPtr = 0;
+    fsps_config.ReservedFspsUpd = [0u8];
+
     // TODO: This struct has to be aligned to 4.
     // mut because we can't make the assumption FSP won't modify it.
-    let mut fsps_upd = fsp64::FSPS_UPD {
-        FspUpdHeader: fsp64::FSP_UPD_HEADER {
-            Signature: fsp64::FSPS_UPD_SIGNATURE,
+    let mut fsps_upd = fsp32::FSPS_UPD {
+        FspUpdHeader: fsp32::FSP_UPD_HEADER {
+            Signature: fsp32::FSPS_UPD_SIGNATURE,
             Revision: 2, // FSP 2.2
             Reserved: [0u8; 23],
         },
-        UnusedUpdSpace0: [0u8; 32],
-        FspsConfig: fsp64::FSP_S_CONFIG {
-            LogoSize: 0,
-            LogoPtr: 0,
-            GraphicsConfigPtr: 0,
-            PciTempResourceBase: 0,
-            UnusedUpdSpace1: [0; 32],
-            ReservedFspsUpd: 0,
-        },
-        UnusedUpdSpace2: [0u8; 13],
+        FspsConfig: fsps_config,
+        FspsTestConfig: fsps_test_config,
         UpdTerminator: 0x55AA, // ???
     };
 
-    let status = unsafe {
+    unsafe {
         type FspSiliconInit =
-            unsafe extern "win64" fn(FspsUpdDataPtr: *mut core::ffi::c_void) -> fsp64::EFI_STATUS;
+            unsafe extern "win64" fn(FspsUpdDataPtr: *mut core::ffi::c_void) -> u32;
         let fsps = core::mem::transmute::<usize, FspSiliconInit>(fsp_base + fsps_entry);
         fsps(core::mem::transmute(&mut fsps_upd))
-    };
-
-    status
+    }
 }
 
 #[no_mangle]
@@ -112,7 +122,9 @@ pub extern "C" fn _start(_fdt_address: usize) -> ! {
     uart0.init().unwrap();
     uart0.pwrite(b"Welcome to oreboot\r\n", 0).unwrap();
     debug.init().unwrap();
-    debug.pwrite(b"Welcome to oreboot - debug port E9\r\n", 0);
+    debug
+        .pwrite(b"Welcome to oreboot - debug port E9\r\n", 0)
+        .unwrap();
 
     let w = &mut print::WriteTo::new(uart0);
 
--- src/vendorcode/fsp/coffeelake/src/lib.rs
+++ src/vendorcode/fsp/coffeelake/src/lib.rs
@@ -3,10 +3,16 @@
 #![allow(non_snake_case)]
 #![no_std]
 
-// Rust types are used instead of generated ones.
-#[repr(C)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct EFI_GUID(pub UINT32, pub UINT16, pub UINT16, pub [UINT8; 8]);
-pub type GUID = EFI_GUID;
-
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+// Don't mangle as this is referenced from a linker script to place in a specific location in
+// flash.
+macro_rules! blob_macro {
+    () => {
+        include_bytes!(concat!(env!("OUT_DIR"), "/", "FSP.fd"))
+    };
+}
+#[no_mangle]
+#[used]
+#[link_section = ".fspblob"]
+static FSP_BLOB: [u8; blob_macro!().len()] = *blob_macro!();
