e37a19a1ce3b6d2f390298fc4f9e88c6ab1cb49b
Jordan Hand <jhand@google.com>
2021-11-05 21:15:36 +0000

    FSP: Organize UPD parameters by arch instead of board
    Refactor the boards which use FSP so that the FSP UPD structures are
    kept in architecture-specific crates instead of board-specific crates.
    This allow boards to share the same parameters for the same CPU
    generation.
    
    Signed-off-by: Jordan Hand <jhand@google.com>

--- src/mainboard/aaeon/upxtreme/src/main.rs
+++ src/mainboard/aaeon/upxtreme/src/main.rs
@@ -16,15 +16,7 @@ use uart::i8250::I8250;
 
 use rpp_procedural::preprocess_asm;
 
-use fsp_cfl_sys as fsp32;
-
-// TODO: it might be nice to do some trickery where fsp_*_sys just exposes
-// everything in fsp_common.
 use fsp_common as fsp;
-
-// Unless we mention the fsp_cfl_sys crate, the compiler will optimize it away. This crate
-// introduces the symbols containing the FSP binary which get picked up by the linker.
-extern crate fsp_cfl_sys;
 
 global_asm!(
     preprocess_asm!("../../../arch/x86/x86_64/src/bootblock_nomem.S"),
@@ -32,45 +24,7 @@ global_asm!(
 );
 
 fn call_fspm(fsp_base: usize, fspm_entry: usize) -> u32 {
-    let mut fspm_cfg = fsp32::FSP_M_CONFIG::default();
-    let mut fspm_test_cfg = fsp32::FSP_M_TEST_CONFIG::default();
-
-    // These values are taken from
-    // https://github.com/coreboot/coreboot/blob/master/src/soc/intel/cannonlake/romstage/fsp_params.c#L20
-    // TODO: Fill out the remaining parameters
-    // TODO: Use constant defines for these instead of magic numbers. For example, define TsegSize
-    // as CONFIG_SMM_TSEG_SIZE. This can be defined in fsp_cfl_sys.
-    fspm_cfg.InternalGfx = 0;
-    fspm_cfg.IgdDvmt50PreAlloc = 0;
-    fspm_cfg.TsegSize = 0x800000;
-    fspm_cfg.IedSize = 0x400000;
-    fspm_cfg.SaGv = 0; // Allows memory training to happen at different frequencies? Disabled for now
-
-    fspm_test_cfg.PanelPowerEnable = 0;
-
-    // TODO: This struct has to be aligned to 4.
-    // mut because we can't make the assumption FSP won't modify it.
-    let mut fspm_upd = fsp32::FSPM_UPD {
-        FspUpdHeader: fsp32::FSP_UPD_HEADER {
-            Signature: fsp32::FSPM_UPD_SIGNATURE,
-            Revision: 2, // FSP 2.2
-            Reserved: [0u8; 23],
-        },
-        FspmArchUpd: fsp32::FSPM_ARCH_UPD {
-            Revision: 2, // FSP 2.2
-            Reserved: [0u8; 3],
-            NvsBufferPtr: 0 as *mut core::ffi::c_void, // non-volatile storage not available
-            StackBase: 0x20000000 as *mut core::ffi::c_void, // TODO: I picked this at random
-            StackSize: 0x10000,                        // TODO: I picked this at random
-            BootLoaderTolumSize: 0, // Don't reserve "top of low usable memory" for bootloader.
-            BootMode: fsp32::BOOT_WITH_FULL_CONFIGURATION,
-            Reserved1: [0u8; 8],
-        },
-        FspmConfig: fspm_cfg,
-        FspmTestConfig: fspm_test_cfg,
-        UnusedUpdSpace6: 0u8,
-        UpdTerminator: 0x55AA, // ???
-    };
+    let mut fspm_upd = fsp_cfl_sys::get_fspm_upd();
 
     unsafe {
         type FspMemoryInit = unsafe extern "win64" fn(
@@ -83,27 +37,7 @@ fn call_fspm(fsp_base: usize, fspm_entry: usize) -> u32 {
 }
 
 fn call_fsps(fsp_base: usize, fsps_entry: usize) -> u32 {
-    let mut fsps_config = fsp32::FSP_S_CONFIG::default();
-    let fsps_test_config = fsp32::FSP_S_TEST_CONFIG::default();
-
-    fsps_config.LogoSize = 0;
-    fsps_config.LogoPtr = 0;
-    fsps_config.GraphicsConfigPtr = 0;
-    fsps_config.ReservedFspsUpd = [0u8];
-
-    // TODO: This struct has to be aligned to 4.
-    // mut because we can't make the assumption FSP won't modify it.
-    let mut fsps_upd = fsp32::FSPS_UPD {
-        FspUpdHeader: fsp32::FSP_UPD_HEADER {
-            Signature: fsp32::FSPS_UPD_SIGNATURE,
-            Revision: 2, // FSP 2.2
-            Reserved: [0u8; 23],
-        },
-        FspsConfig: fsps_config,
-        FspsTestConfig: fsps_test_config,
-        UpdTerminator: 0x55AA, // ???
-    };
-
+    let mut fsps_upd = fsp_cfl_sys::get_fsps_upd();
     unsafe {
         type FspSiliconInit =
             unsafe extern "win64" fn(FspsUpdDataPtr: *mut core::ffi::c_void) -> u32;
