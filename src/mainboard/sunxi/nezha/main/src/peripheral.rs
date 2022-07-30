815ae85f5f35ca51ad037ed29b52e074d9abe5ea
Daniel Maslowski <info@orangecms.org>
2022-07-30 21:04:57 +0200

    sunxi/nezha: set_timer fixup
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/mainboard/sunxi/nezha/main/src/peripheral.rs
+++ src/mainboard/sunxi/nezha/main/src/peripheral.rs
@@ -1,4 +1,5 @@
-use riscv::register::mip;
+use core::arch::asm;
+use riscv::register::{mie, mip};
 use rustsbi::{print, HartMask, SbiRet};
 
 use oreboot_soc::sunxi::d1::clint::{msip, mtimecmp};
@@ -31,14 +32,22 @@ impl rustsbi::Ipi for Ipi {
 struct Timer;
 impl rustsbi::Timer for Timer {
     fn set_timer(&self, stime_value: u64) {
+        let time: u64;
+        unsafe {
+            asm!("csrr {}, time", out(reg) time);
+        }
         // FIXME: This is an attempt to see if the timer is an issue; remove!
-        let stime_value = stime_value >> 8;
         print!("[rustsbi] setTimer {}\n", stime_value);
         mtimecmp::write(stime_value);
         unsafe {
             // clear the pending timer interrupt bit as well.
-            mip::clear_mtimer();
-            mip::set_mtimer()
+            mip::set_mtimer();
+            if time > stime_value {
+                mip::set_stimer();
+            } else {
+                mip::clear_stimer();
+                mie::set_mtimer();
+            }
         };
     }
 }
