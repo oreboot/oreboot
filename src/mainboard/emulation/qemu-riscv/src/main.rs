22a6def9840ae9cf5988df539f5f68a4ec61a31b
Ronald G. Minnich <rminnich@google.com>
2022-11-01 18:51:00 +0000

    use the standard crate for ns16550
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/mainboard/emulation/qemu-riscv/src/main.rs
+++ src/mainboard/emulation/qemu-riscv/src/main.rs
@@ -5,15 +5,25 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use uart_16550::MmioSerialPort;
+
+const SERIAL_PORT_BASE_ADDRESS: usize = 0x1000_0000;
+
+// To receive a byte:
+//let data = serial_port.receive();
 
 #[no_mangle]
 pub extern "C" fn _start(fdt_address: usize) -> ! {
-    loop{
+    loop {
+        let mut serial_port = unsafe { MmioSerialPort::new(SERIAL_PORT_BASE_ADDRESS) };
+        serial_port.init();
+
+        // Now the serial port is ready to be used. To send a byte:
+        serial_port.send(42);
     }
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop{
-    }
+    loop {}
 }
