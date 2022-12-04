e553cfe963059a65e89e792ef720f44845ec7366
rmsyn <rmsynchls@gmail.com>
2022-12-04 16:33:04 -0700

    consts: add GPIO related constants
    Signed-off-by: rmsyn <rmsynchls@gmail.com>

--- src/lib/consts/src/lib.rs
+++ src/lib/consts/src/lib.rs
@@ -14,3 +14,48 @@ pub use units::*;
 pub enum Baud {
     B115200,
 }
+
+#[cfg(__SMM__)]
+pub const ENV_SMM: bool = true;
+#[cfg(not(__SMM__))]
+pub const ENV_SMM: bool = false;
+
+#[cfg(__DECOMPRESSOR__)]
+pub const ENV_DECOMPRESSOR: bool = true;
+#[cfg(not(__DECOMPRESSOR__))]
+pub const ENV_DECOMPRESSOR: bool = false;
+
+#[cfg(__BOOTBLOCK__)]
+pub const ENV_BOOTBLOCK: bool = true;
+#[cfg(not(__BOOTBLOCK__))]
+pub const ENV_BOOTBLOCK: bool = false;
+
+#[cfg(__ROMSTAGE__)]
+pub const ENV_ROMSTAGE: bool = true;
+#[cfg(not(__ROMSTAGE__))]
+pub const ENV_ROMSTAGE: bool = false;
+
+#[cfg(__RAMSTAGE__)]
+pub const ENV_RAMSTAGE: bool = true;
+#[cfg(not(__RAMSTAGE__))]
+pub const ENV_RAMSTAGE: bool = false;
+
+#[cfg(__SEPARATE_VERSTAGE__)]
+pub const ENV_SEPARATE_VERSTAGE: bool = true;
+#[cfg(not(__SEPARATE_VERSTAGE__))]
+pub const ENV_SEPARATE_VERSTAGE: bool = false;
+
+#[cfg(__RMODULE__)]
+pub const ENV_RMODULE: bool = true;
+#[cfg(not(__RMODULE__))]
+pub const ENV_RMODULE: bool = false;
+
+#[cfg(__POSTCAR__)]
+pub const ENV_POSTCAR: bool = true;
+#[cfg(not(__POSTCAR__))]
+pub const ENV_POSTCAR: bool = false;
+
+pub const ENV_ROMSTAGE_OR_BEFORE: bool = ENV_DECOMPRESSOR
+    || ENV_BOOTBLOCK
+    || ENV_ROMSTAGE
+    || (ENV_SEPARATE_VERSTAGE && !cfg!(VBOOT_STARTS_IN_ROMSTAGE));
