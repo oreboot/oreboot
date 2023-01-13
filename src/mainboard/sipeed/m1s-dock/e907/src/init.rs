baa07d19c38128820a0d96edc3f2cd8090b93410
Daniel Maslowski <info@orangecms.org>
2023-01-13 15:15:45 +0100

    sipeed/m1s-dock: reduce loop, resume D0 and reset M0 after it
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/mainboard/sipeed/m1s-dock/e907/src/init.rs
+++ src/mainboard/sipeed/m1s-dock/e907/src/init.rs
@@ -4,10 +4,26 @@ const GLB_BASE: usize   = 0x2000_0000;
 pub const SWRST_CFG2: usize = GLB_BASE + 0x0548;
 const CPU_RST: u32 = 1 << 1;
 
+const MM_GLB_BASE: usize = 0x3000_7000;
+const MM_SW_SYS_RESET: usize = MM_GLB_BASE + 0x0040;
+const SYS_RESET: u32 = 1 << 0;
+const PWRON_RST: u32 = 1 << 2;
+const MMCPU0_RESET: u32 = 1 << 8;
+const MMCPU1_RESET: u32 = 1 << 9;
+const WL2MM_RST_MSK: u32 = 1 << 15;
+const MM_RESUME: u32 = SYS_RESET | PWRON_RST | MMCPU1_RESET | WL2MM_RST_MSK;
+
 pub fn reset_cpu() {
     unsafe {
         let s = core::ptr::read_volatile(SWRST_CFG2 as *mut u32);
         core::ptr::write_volatile(SWRST_CFG2 as *mut u32, s | CPU_RST);
+    }
+}
+
+pub fn resume_mm() {
+    unsafe {
+        let s = core::ptr::read_volatile(MM_SW_SYS_RESET as *mut u32);
+        core::ptr::write_volatile(MM_SW_SYS_RESET as *mut u32, s & MM_RESUME);
     }
 }
 
