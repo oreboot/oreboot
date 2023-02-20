b6fb6f54c5b551c878550e0a7b208108d3afa9ea
Daniel Maslowski <info@orangecms.org>
2023-02-20 01:27:03 +0100

    Bootstrap mainboard/sunxi/H616
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

---
+++ src/mainboard/sunxi/H616/README.md
@@ -0,0 +1,38 @@
+# Allwinner H616
+
+## Developing oreboot
+
+TL;DR `make run`
+
+We currently use [`xfel`](https://github.com/xboot/xfel) to load our code over
+USB. [FEL](https://linux-sunxi.org/FEL) is Allwinner's mask ROM loader.
+
+Goal: initialize the [DRAM controller](https://linux-sunxi.org/DRAM_Controller).
+
+## Boards
+
+There are many development boards.
+
+The oreboot contributors are currently using the following:
+
+### [MangoPi MQ-Quad](https://mangopi.org/mangopi_mqquad)
+
+We assign UART0 to the GPIO pins that a commonly used for RPi-like headers.
+I.e., pin 8 is TX, pin 10 is RX.
+
+See also: <https://linux-sunxi.org/MangoPi_MQ-Quad>
+
+### [KickPI K2B](https://www.kickpi.com/product/k2b/)
+
+We assign UART0 to the GPIO pins that are labeled for it on the 2x10-pin header.
+I.e., pin 17 is TX, pin 15 is RX.
+
+See also: <https://linux-sunxi.org/Kickpi_K2B_H618>
+
+### [BigTreeTech Pi 1.2](https://github.com/bigtreetech/BTT-Pi/)
+
+A USB serial converter is already on the board, accessible via the USB-C port.
+We assign UART0 to it.
+FEL is on the top USB-A port in the middle. You can use a USB A-to-A cable.
+
+See also: <https://linux-sunxi.org/Biqu_BTT_Pi>
---
+++ src/mainboard/sunxi/H616/bt32/src/main.rs
@@ -0,0 +1,385 @@
+#![no_std]
+#![no_main]
+#![feature(once_cell_get_mut)]
+
+use core::arch::naked_asm;
+use core::{arch::asm, panic::PanicInfo};
+
+use embedded_hal_nb::serial::Write;
+use util::mmio::{read32, write32};
+
+#[macro_use]
+extern crate log;
+mod mem_map;
+mod uart;
+
+use mem_map::CCU_BASE;
+
+const STACK_SIZE: usize = 1 * 2048; // 1KiB
+
+#[link_section = ".bss.uninit"]
+static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
+
+/*
+// FIXME: The compiler would add a `BRK` (aarch64) instruction hereafter.
+// FIXME: The compiler would add a `UDF` (aarch32) instruction hereafter.
+// No clue why, so just add `jump` as an inline word to eGON header instead.
+/// Jump over head data to executable code.
+///
+/// # Safety
+///
+/// Naked function.
+#[unsafe(naked)]
+#[link_section = ".head.text"]
+#[export_name = "head_jump"]
+pub unsafe extern "C" fn head_jump() {
+    asm!(
+        "b .+0x64", // 0x60: eGON.BT0 header; 0x08: FlashHead
+        // ".word 0x54000341", // this is the result...
+        options(noreturn)
+    )
+}
+*/
+// eGON.BT0 header. This header is identified by D1 ROM code
+// to copy BT0 stage bootloader into SRAM memory.
+// This header takes 0x60 bytes.
+#[repr(C)]
+pub struct EgonHead {
+    jump32: u32,
+    magic: [u8; 8],
+    checksum: u32,
+
+    length: u32,
+    pub_head_size: u32,
+    fel_script_address: u32,
+    fel_uenv_length: u32,
+
+    dt_name_offset: u32,
+    dram_size: u32,
+    boot_media: u32,
+
+    string_pool: [u32; 13],
+}
+
+const STAMP_CHECKSUM: u32 = 0x5F0A6C39;
+
+// clobber used by KEEP(*(.head.egon)) in link script
+#[link_section = ".head.egon"]
+pub static EGON_HEAD: EgonHead = EgonHead {
+    jump32: 0xea000016, // b 0x60
+    magic: *b"eGON.BT0",
+    checksum: STAMP_CHECKSUM, // real checksum filled by blob generator
+    length: 0,                // real size filled by blob generator
+    pub_head_size: 0,
+    fel_script_address: 0,
+    fel_uenv_length: 0,
+    dt_name_offset: 0,
+    dram_size: 0,
+    boot_media: 0,
+    string_pool: [0; 13],
+};
+
+// Private use; not designed as conventional header structure.
+// Real data filled by xtask.
+// This header takes 0x8 bytes. When modifying this structure, make sure
+// the offset in `head_jump` function is also modified.
+#[repr(C)]
+pub struct MainStageHead {
+    offset: u32,
+    length: u32,
+}
+
+// clobber used by KEEP(*(.head.main)) in link script
+// To avoid optimization, always read from flash page. Do NOT use this
+// variable directly.
+#[link_section = ".head.main"]
+pub static MAIN_STAGE_HEAD: MainStageHead = MainStageHead {
+    offset: 0, // real offset filled by xtask
+    length: 0, // real size filled by xtask
+};
+
+/// All 64-bit capable Allwinner SoCs reset in AArch32 (and continue to
+/// exectute the Boot ROM in this state), so we need to switch to AArch64
+/// at some point.
+/// https://github.com/u-boot/u-boot/blob/master/arch/arm/mach-sunxi/rmr_switch.S
+///
+/// Clear stuff and jump to main.
+/// Kudos to Azeria \o/
+/// https://azeria-labs.com/memory-instructions-load-and-store-part-4/
+/// Xn registers are 64-bit, general purpose; X31 aka Xzr is always 0
+/// Wn registers are 32-bit and aliases of lower half of Xn
+/// https://linux-sunxi.org/Arm64
+///
+/// # Safety
+///
+/// Naked function.
+#[unsafe(naked)]
+#[export_name = "start"]
+#[link_section = ".text.entry"]
+pub unsafe extern "C" fn start() -> ! {
+    naked_asm!(
+        "2:",             //
+        "adr  r9, 2b", //
+        "bl   {reset}",
+        ".word 0x140001e7", // b #0x490 in Aarch64
+        // hack to include eGON header; we skip this via the jump from header
+        "ldr  r0, {egon_head}",
+        egon_head  =   sym EGON_HEAD,
+        reset      =   sym reset
+    )
+}
+
+// see "Port controller" in the manual
+const GPIO_BASE: usize = 0x0300_B000;
+const GPIO_PORTC_CFG1: usize = GPIO_BASE + 0x004C; // PC8-15
+const GPIO_PORTC_DATA: usize = GPIO_BASE + 0x0058;
+const GPIO_PORTH_CFG0: usize = GPIO_BASE + 0x00FC;
+const GPIO_PORTH_PULL: usize = GPIO_BASE + 0x0118;
+
+const PC13_OUT: u32 = 0b001 << 20;
+const PC13_HIGH: u32 = 1 << 13;
+
+const APB2_CFG_REG: usize = CCU_BASE + 0x0524;
+const UART_BGR_REG: usize = CCU_BASE + 0x090C;
+
+// See U-Boot arch/arm/mach-sunxi/Kconfig
+//   under config MACH_SUN50I_H6(16), there is select SUN50I_GEN_H6
+// for H6 targets
+const RVBAR: usize = 0x0901_0040;
+// non-H6 targets
+// const RVBAR: usize = 0x0170_00a0;
+
+const RVBAR_ALT: usize = 0x0810_0040;
+// for non-H6
+// const RVBAR_ALT: usize = RVBAR;
+
+const START_AARCH64: u32 = 0x0002_0000 + 2048;
+
+fn sleep(t: usize) {
+    for _ in 0..t {
+        core::hint::spin_loop();
+    }
+}
+
+// blink the LED on the MangoPi MQ-Quad
+fn blink(delay: usize) {
+    let cycs = delay * 0x10000;
+    write32(GPIO_PORTC_DATA, PC13_HIGH);
+    sleep(cycs);
+    write32(GPIO_PORTC_DATA, 0);
+    sleep(cycs);
+}
+
+#[inline]
+fn save_regs() {
+    #[allow(named_asm_labels)]
+    unsafe {
+        asm!(
+            "b .code",
+            // leave space to store information
+            ".fel_stash: ",
+            ".word 0x00000000", // SP
+            ".word 0x00000000", // LR
+            ".word 0x00000000", // CPSR
+            ".word 0x00000000", // SCTLR
+            ".word 0x00000000", // VBAR
+            ".word 0x00000000", // SP_IRQ
+            ".word 0x00000000", // ICC_PMR
+            ".word 0x00000000", // ICC_IGRPEN1
+            ".code:",
+            "adr     r0, .fel_stash",
+            "ldr     r1, .fel_stash",
+            "add     r0, r0, r1",
+            "str     sp, [r0]",
+            "str     lr, [r0, #4]",
+            "mrs     lr, CPSR",
+            "str     lr, [r0, #8]",
+            "mrc     p15, 0, lr, cr1, cr0, 0", // SCTLR
+            "str     lr, [r0, #12]",
+            "mrc     p15, 0, lr, cr12, cr0, 0", // VBAR
+            "str     lr, [r0, #16]",
+            "mrc     p15, 0, lr, cr12, cr12, 5", // ICC_SRE
+            "tst     lr, #1",
+            "beq     2f",
+            "mrc     p15, 0, lr, c4, c6, 0", // ICC_PMR
+            "str     lr, [r0, #24]",
+            "mrc     p15, 0, lr, c12, c12, 7", // ICC_IGRPEN1
+            "str     lr, [r0, #28]",
+            "2:"
+        );
+    }
+}
+
+#[inline]
+fn reset64() {
+    if false {
+        write32(RVBAR, START_AARCH64);
+    } else {
+        write32(RVBAR_ALT, START_AARCH64);
+    }
+    unsafe {
+        asm!(
+            "dsb	sy",
+            "isb	sy",
+            "mrc	p15, 0, r0, cr12, cr0, 2", // read RMR register
+            "orr	r0, r0, #3",               // request reset in AArch64
+            "mcr	p15, 0, r0, cr12, cr0, 2", // write RMR register
+            "isb	sy",
+        );
+    }
+}
+
+// crappy debug prints
+fn pchar(c: u32) {
+    write32(mem_map::UART0_BASE, c);
+    sleep(400);
+}
+
+const NEW_METHOD: bool = true;
+
+// FIXME: both methods fail at the moment. Why?
+fn init_logger(s: uart::SunxiSerial) {
+    pchar(0x30);
+    if NEW_METHOD {
+        // This is the new method that also compiles in Rust 2024.
+        use core::{cell::OnceCell, ptr::addr_of_mut};
+        static mut SERIAL: OnceCell<uart::SunxiSerial> = OnceCell::new();
+        pchar(0x31);
+        unsafe {
+            log::init((*addr_of_mut!(SERIAL)).get_mut_or_init(|| s));
+        }
+    } else {
+        // This is the old method we've been using for now.
+        static ONCE: spin::Once<()> = spin::Once::new();
+        static mut SERIAL: Option<uart::SunxiSerial> = None;
+        ONCE.call_once(|| unsafe {
+            pchar(0x31);
+            SERIAL.replace(s);
+            pchar(0x32);
+            log::init(SERIAL.as_mut().unwrap());
+        });
+    }
+    pchar(0x42);
+}
+
+#[inline(always)]
+// shift n by s and convert to what represents its hex digit in ASCII
+fn shift_and_hex(n: u32, s: u8) -> u8 {
+    // drop to a single nibble (4 bits), i.e., what a hex digit can hold
+    let x = (n >> s) as u8 & 0x0f;
+    // digits are in the range 0x30..0x39
+    // letters start at 0x40, i.e., off by 7 from 0x3a
+    if x > 9 {
+        x + 0x37
+    } else {
+        x + 0x30
+    }
+}
+
+#[inline(always)]
+pub fn print_hex(s: &mut uart::SunxiSerial, i: u32) {
+    s.write(b'0').ok();
+    s.write(b'x').ok();
+    // nibble by nibble... keep it simple
+    s.write(shift_and_hex(i, 28)).ok();
+    s.write(shift_and_hex(i, 24)).ok();
+    s.write(shift_and_hex(i, 20)).ok();
+    s.write(shift_and_hex(i, 16)).ok();
+    s.write(shift_and_hex(i, 12)).ok();
+    s.write(shift_and_hex(i, 8)).ok();
+    s.write(shift_and_hex(i, 4)).ok();
+    s.write(shift_and_hex(i, 0)).ok();
+    s.write(b'\r').ok();
+    s.write(b'\n').ok();
+}
+
+// #[unsafe(naked)]
+#[no_mangle]
+unsafe extern "C" fn reset() {
+    // stack setup
+    asm!("mov sp, {}", in(reg) &raw const STACK);
+    asm!(
+        "ldr  r1, ={stack_size}",
+        "add  sp, r1",
+        "bl   {main}",
+        stack_size = const STACK_SIZE,
+        main       = sym main
+    );
+}
+
+const PRINT_PC: bool = false;
+const PRINT_SP: bool = true;
+
+// see also https://iitd-plos.github.io/col718/ref/arm-instructionset.pdf
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    let mut ini_pc: usize = 0;
+    unsafe { asm!("mov {}, r9", out(reg) ini_pc) };
+    let mut ini_sp: usize = 0;
+    unsafe { asm!("mov {}, sp", out(reg) ini_sp) };
+
+    // System init: select APB@24MHz
+    let v = read32(APB2_CFG_REG) & !(0b11 << 24);
+    write32(APB2_CFG_REG, v);
+
+    // set PC13 (status LED) to output
+    write32(GPIO_PORTC_CFG1, PC13_OUT);
+    // first sign of life
+    blink(5);
+
+    // UART0: TX on port H pin 0, RX on port H pin 1
+    let v = read32(GPIO_PORTH_CFG0) & 0xffff_ff00;
+    write32(GPIO_PORTH_CFG0, v | (0b010 << 4) | (0b010 << 0));
+    let v = read32(GPIO_PORTH_PULL) & 0xffff_fff0;
+    write32(GPIO_PORTH_PULL, v | (0b01 << 2) | (0b01 << 0));
+
+    const UART0_GATING: u32 = 1 << 16;
+    const UART0_RESET: u32 = 1 << 0;
+    // deassert reset
+    let v = read32(UART_BGR_REG) & !UART0_GATING;
+    write32(UART_BGR_REG, v | UART0_GATING);
+    // gating pass
+    let v = read32(UART_BGR_REG) & !UART0_RESET;
+    write32(UART_BGR_REG, v | UART0_RESET);
+
+    let mut serial = uart::SunxiSerial::new();
+    blink(5);
+
+    serial.write(b'\r').ok();
+    serial.write(b'\n').ok();
+
+    if PRINT_PC {
+        // this verifies that our base address is correct
+        // should be SRAM base address + 0x60, i.e., right after eGON header
+        serial.write(b'P').ok();
+        serial.write(b'C').ok();
+        serial.write(b':').ok();
+        serial.write(b' ').ok();
+        print_hex(&mut serial, ini_pc as u32);
+    }
+
+    if PRINT_SP {
+        serial.write(b'S').ok();
+        serial.write(b'P').ok();
+        serial.write(b':').ok();
+        serial.write(b' ').ok();
+        print_hex(&mut serial, ini_sp as u32);
+    }
+
+    // currently crashes here, as it seems
+    init_logger(serial);
+    // println!("🦀");
+    // println!("oreboot 🦀 in aarch32");
+    // println!("earlier program counter (PC) {ini_pc:016x}");
+
+    loop {
+        blink(42);
+    }
+}
+
+#[cfg_attr(not(test), panic_handler)]
+fn panic(info: &PanicInfo) -> ! {
+    loop {
+        core::hint::spin_loop();
+    }
+}
