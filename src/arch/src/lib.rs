ce460c022fafa7b638fd588a9809e8b0a4ba35ad
Ronald G. Minnich <rminnich@gmail.com>
2023-05-22 21:45:06 +0000

    remove ppc64
    This is easier than removing all of src/arch at once.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/arch/src/lib.rs
+++ src/arch/src/lib.rs
@@ -1,7 +1,5 @@
 #![feature(asm_const)]
 #![no_std]
 
-#[cfg(feature = "powerpc64")]
-pub mod ppc64;
 #[cfg(feature = "riscv64")]
 pub mod riscv64;
--- src/arch/src/ppc64/mod.rs
+++
@@ -1,3 +0,0 @@
-pub fn halt() -> ! {
-    panic!("Implement halt");
-}
