a88acb7a56b5cb8cef0bfad8c40a389462a0aace
Daniel Maslowski <info@orangecms.org>
2023-06-22 00:24:34 +0200

    arch/riscv64: WIP: debug and rework SBI
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/arch/src/riscv64/sbi/info.rs
+++ src/arch/src/riscv64/sbi/info.rs
@@ -1,4 +1,4 @@
-use super::hart_csr_utils;
+use super::csr;
 use log::println;
 
 pub fn print_info(platform: &str, version: &str) {
@@ -6,6 +6,5 @@ pub fn print_info(platform: &str, version: &str) {
     println!("{}", rustsbi::LOGO);
     println!("Platform Name: {}", platform);
     println!("Implementation: oreboot version {}", version);
-    hart_csr_utils::print_hart_csrs();
-    hart_csr_utils::print_hart_pmp();
+    csr::print_info();
 }
