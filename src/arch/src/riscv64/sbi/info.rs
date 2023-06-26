acd0ea05421ff0ddb945ef4660accd3f24cae0ea
Daniel Maslowski <info@orangecms.org>
2023-06-26 17:00:26 +0200

    VF2, SBI WIP
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/arch/src/riscv64/sbi/info.rs
+++ src/arch/src/riscv64/sbi/info.rs
@@ -4,7 +4,7 @@ use log::println;
 pub fn print_info(platform: &str, version: &str) {
     println!("RustSBI version {}", rustsbi::VERSION);
     println!("{}", rustsbi::LOGO);
-    println!("Platform Name: {}", platform);
-    println!("Implementation: oreboot version {}", version);
+    println!("Platform Name: {platform}");
+    println!("Implementation: oreboot version {version}");
     csr::print_info();
 }
