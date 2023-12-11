23cdba3eff5001d26173ed0937bfb521d910d6cf
Daniel Maslowski <info@orangecms.org>
2023-12-11 22:31:33 +0100

    arch/riscv64: add references to SBI documentation and test suite
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/arch/src/riscv64/mod.rs
+++ src/arch/src/riscv64/mod.rs
@@ -1,3 +1,7 @@
+// For more information on what SBI is and what methods etc are defined, see
+// https://github.com/riscv-non-isa/riscv-sbi-doc/blob/master/riscv-sbi.adoc
+// For a test suite, have a look at https://github.com/rustsbi/sbi-testing
+
 pub mod sbi {
     pub mod csr;
     pub mod execute;
