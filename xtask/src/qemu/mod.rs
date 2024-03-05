1b7a4fa5022d748ebf57b72a0d1ad15269202575
Felipe Balbi <felipe@balbi.sh>
2024-03-05 20:49:30 +0200

    Re-add aarch64 emulation
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- xtask/src/qemu/mod.rs
+++ xtask/src/qemu/mod.rs
@@ -1,3 +1,4 @@
+mod aarch64;
 mod riscv;
 
 use super::Cli;
@@ -5,12 +6,14 @@ use super::Cli;
 #[derive(Debug, Clone, Copy)]
 pub enum Board {
     RiscV,
+    AArch64,
 }
 
 impl Board {
     pub(crate) fn execute_command(self, command: &Cli, features: Vec<String>) {
         match self {
             Board::RiscV => riscv::execute_command(command, features),
+            Board::AArch64 => aarch64::execute_command(command, features),
         }
     }
 }
