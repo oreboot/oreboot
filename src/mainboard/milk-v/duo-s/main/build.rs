9a908297d5c917c2271bdb212062aa6032252a9f
Daniel Maslowski <info@orangecms.org>
2024-05-04 01:07:44 +0200

    CV/SG: simple main stage
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

---
+++ src/mainboard/milk-v/duo-s/main/build.rs
@@ -0,0 +1,113 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+
+// see https://github.com/sophgo/fsbl
+//
+// Earlier SoCs (CV1800B, Milk-V Duo)
+//
+//  plat/cv180x/bl2/bl2.ld.S
+//  19:    RAM (rwx): ORIGIN = BL2_BASE, LENGTH = BL2_SIZE
+//
+//  plat/cv180x/include/mmap.h
+//  48:#define BL2_BASE (VC_RAM_BASE)
+//
+//  plat/cv180x/include/platform_def.h
+//  296:    #define VC_RAM_BASE 0x3BC00000 // Shadow_vc_mem
+//
+// Latere SoCs (SG200x, Milk-V Duo 256M, Duo S)
+//
+//  plat/cv181x/include/platform_def.h
+// #define TPU_SRAM_ORIGIN_BASE 0x0C000000
+// #define TPU_SRAM_SIZE 0x40000 // 256KiB
+
+const LINKERSCRIPT_FILENAME: &str = "link-duo_s-main.ld";
+
+#[cfg(soc = "CV1800B")]
+const LINKERSCRIPT: &[u8] = b"
+OUTPUT_ARCH(riscv)
+ENTRY(_start)
+MEMORY {
+    SRAM : ORIGIN = 0x80000000, LENGTH = 64M
+}
+SECTIONS {
+    .head : {
+        *(.head.text)
+    } > SRAM
+    .text : {
+        KEEP(*(.text.entry))
+        *(.text .text.*)
+        . = ALIGN(8);
+    } > SRAM
+    .bss : {
+        _sbss = .;
+        *(.bss .bss.*);
+        _ebss = .;
+    } > SRAM
+
+    # https://docs.rust-embedded.org/embedonomicon/main.html
+    .rodata : {
+        *(.rodata .rodata.*);
+    } > SRAM #FLASH
+    .data : {
+        _sdata = .;
+        *(.data .data.*);
+        _edata = .;
+    } > SRAM
+    _sidata = LOADADDR(.data);
+
+    /DISCARD/ : {
+        *(.eh_frame)
+        *(.debug_*)
+        *(.comment*)
+    }
+}";
+#[cfg(not(soc = "CV1800B"))]
+const LINKERSCRIPT: &[u8] = b"
+OUTPUT_ARCH(riscv)
+ENTRY(_start)
+MEMORY {
+    SRAM : ORIGIN = 0x80000000, LENGTH = 512M
+}
+SECTIONS {
+    .head : {
+        *(.head.text)
+    } > SRAM
+    .text : {
+        KEEP(*(.text.entry))
+        *(.text .text.*)
+        . = ALIGN(8);
+    } > SRAM
+    .bss : {
+        _sbss = .;
+        *(.bss .bss.*);
+        _ebss = .;
+    } > SRAM
+
+    # https://docs.rust-embedded.org/embedonomicon/main.html
+    .rodata : {
+        *(.rodata .rodata.*);
+    } > SRAM #FLASH
+    .data : {
+        _sdata = .;
+        *(.data .data.*);
+        _edata = .;
+    } > SRAM
+    _sidata = LOADADDR(.data);
+
+    /DISCARD/ : {
+        *(.eh_frame)
+        *(.debug_*)
+        *(.comment*)
+    }
+}";
+
+fn main() {
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    File::create(out.join(LINKERSCRIPT_FILENAME))
+        .unwrap()
+        .write_all(LINKERSCRIPT)
+        .unwrap();
+    println!("cargo:rustc-link-search={}", out.display());
+}
---
+++ src/mainboard/milk-v/duo-s/main/src/main.rs
@@ -0,0 +1,177 @@
+#![feature(naked_functions, asm_const)]
+#![feature(panic_info_message)]
+#![no_std]
+#![no_main]
+// TODO: remove when done debugging crap
+#![allow(unused)]
+
+use embedded_hal_nb::serial::Write;
+
+#[macro_use]
+extern crate log;
+
+use core::{
+    arch::asm,
+    mem::transmute,
+    panic::PanicInfo,
+    ptr::{self, addr_of, addr_of_mut},
+    slice::from_raw_parts as slice_from,
+};
+use riscv::register::mhartid;
+use riscv::register::{marchid, mimpid, mvendorid};
+
+use layoutflash::areas::{find_fdt, FdtIterator};
+
+mod uart;
+
+pub type EntryPoint = unsafe extern "C" fn();
+
+const DEBUG: bool = false;
+
+const STACK_SIZE: usize = 512;
+
+#[link_section = ".bss.uninit"]
+static mut BT0_STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
+
+/// Set up stack and jump to executable code.
+///
+/// # Safety
+///
+/// Naked function.
+#[naked]
+#[export_name = "start"]
+#[link_section = ".text.entry"]
+#[allow(named_asm_labels)]
+pub unsafe extern "C" fn start() -> ! {
+    // starts with a 32 bytes header
+    asm!(
+        "j      .forrealsiez", // 2 bytes with compact instruction
+        ".byte 0",
+        ".byte 0",
+        ".word 0", // resvered
+        ".word 0", // BL2 MSID
+        ".word 0", // BL2 version
+        ".word 0", // rest not documented
+        ".word 0",
+        ".word 0",
+        ".word 0",
+        ".forrealsiez:",
+        "li     t0, 0x04140000",
+        "li     t1, 0x42",
+        "sw     t1, 0(t0)",
+        // Clear feature disable CSR to '0' to turn on all features
+        // TODO: do in Rust
+        // "csrwi  0x7c1, 0",
+        "csrw   mie, zero",
+        "csrw   mstatus, zero",
+        "ld     t0, {start}",
+        "csrw   mtvec, t0",
+        // 1. suspend non-boot hart
+        // hart 0 is the S7 monitor core; 1-4 are U7 cores
+        "li     a1, 1",
+        "csrr   a0, mhartid",
+        "bne    a0, a1, .nonboothart",
+        // 2. prepare stack
+        // FIXME: each hart needs its own stack
+        "la     sp, {stack}",
+        "li     t0, {stack_size}",
+        "add    sp, sp, t0",
+        "j      .boothart",
+        // wait for multihart to get back into the game
+        ".nonboothart:",
+        "j      .boothart",
+        "csrw   mie, 8", // 1 << 3
+        "wfi",
+        "csrw   mip, 0",
+        "call   {payload}",
+        ".boothart:",
+        "call   {reset}",
+        stack      = sym BT0_STACK,
+        stack_size = const STACK_SIZE,
+        payload    = sym exec_payload,
+        reset      = sym reset,
+        start      = sym start,
+        options(noreturn)
+    )
+}
+
+/// Initialize RAM: Clear BSS and set up data.
+/// See https://docs.rust-embedded.org/embedonomicon/main.html
+///
+/// # Safety
+/// :shrug:
+#[no_mangle]
+pub unsafe extern "C" fn reset() {
+    extern "C" {
+        static mut _sbss: u8;
+        static mut _ebss: u8;
+
+        static mut _sdata: u8;
+        static mut _edata: u8;
+        static _sidata: u8;
+    }
+
+    let bss_size = addr_of!(_ebss) as usize - addr_of!(_sbss) as usize;
+    ptr::write_bytes(addr_of_mut!(_sbss), 0, bss_size);
+
+    let data_size = addr_of!(_edata) as usize - addr_of!(_sdata) as usize;
+    ptr::copy_nonoverlapping(addr_of!(_sidata), addr_of_mut!(_sdata), data_size);
+    // Call user entry point
+    main();
+}
+
+static mut SERIAL: Option<uart::SGSerial> = None;
+
+fn init_logger(s: uart::SGSerial) {
+    unsafe {
+        SERIAL.replace(s);
+        if let Some(m) = SERIAL.as_mut() {
+            log::init(m);
+        }
+    }
+}
+
+#[no_mangle]
+fn main() {
+    let s = uart::SGSerial::new();
+    init_logger(s);
+    println!();
+    println!("oreboot 🦀 main");
+
+    unsafe {
+        // reset();
+        riscv::asm::wfi()
+    };
+}
+
+fn exec_payload(addr: usize) {
+    unsafe {
+        // jump to main
+        let f: EntryPoint = transmute(addr);
+        // asm!("fence.i");
+        f();
+    }
+}
+
+#[cfg_attr(not(test), panic_handler)]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        if DEBUG {
+            println!(
+                "[bt0] panic in '{}' line {}",
+                location.file(),
+                location.line(),
+            );
+        }
+    } else {
+        if DEBUG {
+            println!("[bt0] panic at unknown location");
+        }
+    };
+    if let Some(msg) = info.message() {
+        println!("[bt0]   {msg}");
+    }
+    loop {
+        core::hint::spin_loop();
+    }
+}
