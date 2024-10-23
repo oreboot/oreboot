24312de5ff630ae985381f011ffacbc9125a4a83
Daniel Maslowski <info@orangecms.org>
2024-10-23 16:58:04 +0200

    lib/consts: move lint exception to the block it refers to
    This got us a clippy error.
    
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/lib/consts/src/lib.rs
+++ src/lib/consts/src/lib.rs
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 #![no_std]
-#[allow(non_upper_case_globals)]
 
+#[allow(non_upper_case_globals)]
 pub mod units {
     pub const KiB: usize = 1 << 10;
     pub const MiB: usize = 1 << 20;
