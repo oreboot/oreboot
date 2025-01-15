45bfbc3a716dfc663b633c3f6ec1071f2ad14c87
Daniel Maslowski <info@orangecms.org>
2025-01-15 23:35:09 +0100

    sipeed/m1s-dock/c906: use bl-pac, init uart, init first PLLs
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

---
+++ src/mainboard/sipeed/m1s-dock/c906/src/util.rs
@@ -0,0 +1,38 @@
+use core::ptr::{read_volatile, write_volatile};
+
+pub fn write32(reg: usize, val: u32) {
+    unsafe {
+        write_volatile(reg as *mut u32, val);
+    }
+}
+
+pub fn read32(reg: usize) -> u32 {
+    unsafe { read_volatile(reg as *mut u32) }
+}
+
+pub fn dump(addr: usize, length: usize) {
+    let s = unsafe { core::slice::from_raw_parts(addr as *const u8, length) };
+    println!("dump {length} bytes @{addr:x}");
+    for w in s.iter() {
+        print!("{:02x}", w);
+    }
+    println!();
+}
+
+pub fn dump_block(base: usize, size: usize, step_size: usize) {
+    for b in (base..base + size).step_by(step_size) {
+        dump(b, step_size);
+    }
+}
+
+pub fn udelay(t: usize) {
+    unsafe {
+        for _ in 0..t * 3 {
+            riscv::asm::nop();
+        }
+    }
+}
+
+pub fn sleep() {
+    udelay(0x1a000);
+}
