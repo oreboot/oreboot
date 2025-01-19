61739177296dba48b768bf0d5da8763acf82096b
Daniel Maslowski <info@orangecms.org>
2025-01-19 02:16:33 +0100

    sipeed/m1s-dock/c906/psram: cleanup/fixup
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/mainboard/sipeed/m1s-dock/c906/src/init.rs
+++ src/mainboard/sipeed/m1s-dock/c906/src/init.rs
@@ -1,6 +1,7 @@
 use crate::util::{clear_bit, read32, set_bit, sleep, udelay, write32};
 
-const GLB_BASE: usize = 0x20000000;
+const GLB_BASE: usize = 0x2000_0000;
+
 const LDO12_UHS: usize = GLB_BASE + 0x06d0;
 const UHS_PLL_CFG0: usize = GLB_BASE + 0x07d0;
 const UHS_PLL_CFG1: usize = GLB_BASE + 0x07d4;
@@ -9,67 +10,68 @@ const UHS_PLL_CFG5: usize = GLB_BASE + 0x07e4;
 const UHS_PLL_CFG6: usize = GLB_BASE + 0x07e8;
 
 const LDO12_UHS_PU_LDO12_UHS_BIT: u32 = 0;
-const LDO12_UHS_VOUT_SEL_POS: u32 = 20;
-const LDO12_UHS_VOUT_SEL_MASK: u32 = 0b1111;
+const LDO12_UHS_VOUT_SEL_OFFSET: u32 = 20;
+const LDO12_UHS_VOUT_SEL_MASK: u32 = 0b1111 << LDO12_UHS_VOUT_SEL_OFFSET;
 
 const UHS_PLL_CFG0_SDM_RSTB_BIT: u32 = 0;
 const UHS_PLL_CFG0_FBDV_RSTB_BIT: u32 = 2;
 const UHS_PLL_CFG0_PU_UHSPLL_SFREG_BIT: u32 = 9;
 const UHS_PLL_CFG0_PU_UHSPLL_BIT: u32 = 10;
 
+const UHS_PLL_CFG1_EVEN_DIV_EN_BIT: u32 = 7;
+const UHS_PLL_CFG1_EVEN_DIV_RATIO_MASK: u32 = 0b0111_1111;
+const UHS_PLL_CFG1_REFERENCE_DIVIDER_RATIO_OFFSET: u32 = 8;
+const UHS_PLL_CFG1_REFERENCE_DIVIDER_RATIO_MASK: u32 =
+    0b1111 << UHS_PLL_CFG1_REFERENCE_DIVIDER_RATIO_OFFSET;
+const UHS_PLL_CFG1_REFERENCE_CLOCK_SELECT_OFFSET: u32 = 16;
+const UHS_PLL_CFG1_REFERENCE_CLOCK_SELECT_MASK: u32 =
+    0b11 << UHS_PLL_CFG1_REFERENCE_CLOCK_SELECT_OFFSET;
+
+const UHS_PLL_CFG4_SEL_SAMPLE_CLK_MASK: u32 = 0b11;
+
+const UHS_PLL_CFG5_VCO_SPEED_MASK: u32 = 0b111;
+
+const UHS_PLL_CFG6_SD_MIN_MASK: u32 = 0x7_ffff;
+
 pub fn glb_power_up_ldo12uhs() {
     set_bit(LDO12_UHS, LDO12_UHS_PU_LDO12_UHS_BIT);
     udelay(300);
     let ldo12_uhs = read32(LDO12_UHS);
-    write32(
-        LDO12_UHS,
-        (ldo12_uhs & !(LDO12_UHS_VOUT_SEL_MASK)) | (6 << 20),
-    );
+    let m = !LDO12_UHS_VOUT_SEL_MASK;
+    let v = 6 << LDO12_UHS_VOUT_SEL_OFFSET;
+    write32(LDO12_UHS, (ldo12_uhs & m) | v);
     udelay(1);
 }
-
-const UHS_PLL_CFG1_EVEN_DIV_EN: u32 = 1 << 7;
-const UHS_PLL_CFG1_EVEN_DIV_RATIO: u32 = 0b1111111;
 
 pub fn pll() {
     // https://github.com/smaeul/u-boot/tree/bl808
     // 2c940eed61391a1adde52ce61b67bd5994f06866
     // drivers/ram/bflb/psram.c
     let cfg1 = read32(UHS_PLL_CFG1);
-    println!("PLL CFG1: {cfg1:08x}");
-    write32(UHS_PLL_CFG1, (cfg1 & !(0b11 << 16)));
+    let m = !UHS_PLL_CFG1_REFERENCE_CLOCK_SELECT_MASK;
+    write32(UHS_PLL_CFG1, cfg1 & m);
+
     let cfg1 = read32(UHS_PLL_CFG1);
-    println!("PLL CFG1: {cfg1:08x}");
-    write32(UHS_PLL_CFG1, (cfg1 & !(0b1111 << 8)) | (0b0010 << 8));
-    let cfg1 = read32(UHS_PLL_CFG1);
-    println!("PLL CFG1: {cfg1:08x}");
+    let m = !UHS_PLL_CFG1_REFERENCE_DIVIDER_RATIO_MASK;
+    let v = 2 << UHS_PLL_CFG1_REFERENCE_DIVIDER_RATIO_OFFSET;
+    write32(UHS_PLL_CFG1, (cfg1 & m) | v);
 
     let cfg4 = read32(UHS_PLL_CFG4);
-    println!("PLL CFG4: {cfg4:08x}");
-    write32(UHS_PLL_CFG4, (cfg4 & !(0b11)) | 0b10);
-    let cfg4 = read32(UHS_PLL_CFG4);
-    println!("PLL CFG4: {cfg4:08x}");
+    let m = !UHS_PLL_CFG4_SEL_SAMPLE_CLK_MASK;
+    write32(UHS_PLL_CFG4, (cfg4 & m) | 2);
 
     let cfg5 = read32(UHS_PLL_CFG5);
-    println!("PLL CFG5: {cfg5:08x}");
-    write32(UHS_PLL_CFG5, (cfg5 & !(0b111)) | 0b100);
-    let cfg5 = read32(UHS_PLL_CFG5);
-    println!("PLL CFG5: {cfg5:08x}");
+    let m = !UHS_PLL_CFG5_VCO_SPEED_MASK;
+    write32(UHS_PLL_CFG5, (cfg5 & m) | 4);
 
     let cfg1 = read32(UHS_PLL_CFG1);
-    println!("PLL CFG1: {cfg1:08x}");
-    let m = !(UHS_PLL_CFG1_EVEN_DIV_EN | UHS_PLL_CFG1_EVEN_DIV_RATIO);
-    write32(UHS_PLL_CFG1, (cfg1 & m) | UHS_PLL_CFG1_EVEN_DIV_EN | 28);
-    let cfg1 = read32(UHS_PLL_CFG1);
-    println!("PLL CFG1: {cfg1:08x}");
+    let m = !((1 << UHS_PLL_CFG1_EVEN_DIV_EN_BIT) | UHS_PLL_CFG1_EVEN_DIV_RATIO_MASK);
+    let v = (1 << UHS_PLL_CFG1_EVEN_DIV_EN_BIT) | 28;
+    write32(UHS_PLL_CFG1, (cfg1 & m) | v);
 
-    const UHS_PLL_CFG6_SDMIN: u32 = 0x7_ffff;
     let cfg6 = read32(UHS_PLL_CFG6);
-    println!("PLL CFG6: {cfg6:08x}");
-    let m = !(UHS_PLL_CFG6_SDMIN);
+    let m = !(UHS_PLL_CFG6_SD_MIN_MASK);
     write32(UHS_PLL_CFG6, (cfg6 & m) | 143360);
-    let cfg6 = read32(UHS_PLL_CFG6);
-    println!("PLL CFG6: {cfg6:08x}");
 
     set_bit(UHS_PLL_CFG0, UHS_PLL_CFG0_PU_UHSPLL_SFREG_BIT);
     udelay(3);
