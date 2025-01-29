c9182c88e479272245f6e50fe981def9859235e1
Daniel Maslowski <info@orangecms.org>
2025-01-29 23:29:18 +0100

    sipeed/m1sdock/c906: move memtest to its own file, silence Clippy
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/mainboard/sipeed/m1s-dock/c906/src/main.rs
+++ src/mainboard/sipeed/m1s-dock/c906/src/main.rs
@@ -16,6 +16,7 @@ use bl808_pac::Peripherals;
 extern crate log;
 
 mod init;
+mod memtest;
 mod psram;
 mod uart;
 mod util;
@@ -84,51 +85,6 @@ fn init_logger(s: uart::BSerial) {
     }
 }
 
-fn check32(addr: usize, val: u32) {
-    let v = read32(addr);
-    if v != val {
-        println!("Error @ {addr:08x}: expected {val:08x}, got {v:08x}");
-    }
-}
-
-const DRAM_TEST_PATTERN_0: u32 = 0x2233_ccee;
-const DRAM_TEST_PATTERN_1: u32 = 0x5577_aadd;
-const DRAM_TEST_PATTERN_2: u32 = 0x1144_bbff;
-const DRAM_TEST_PATTERN_3: u32 = 0x6688_9900;
-
-fn dram_test(base: usize, size: usize) {
-    let limit = base + size;
-    let step_size = 0x10;
-    // print 64 steps, which gets slower with a higher size to test
-    let print_step = size / step_size / 64;
-
-    println!("DRAM test: write patterns...");
-    for (i, a) in (base..limit).step_by(step_size).enumerate() {
-        if i % print_step == 0 {
-            print!(".");
-        }
-        write32(a + 0x0, DRAM_TEST_PATTERN_0 | i as u32);
-        write32(a + 0x4, DRAM_TEST_PATTERN_1 | i as u32);
-        write32(a + 0x8, DRAM_TEST_PATTERN_2 | i as u32);
-        write32(a + 0xc, DRAM_TEST_PATTERN_3 | i as u32);
-    }
-    println!();
-
-    println!("DRAM test: reading back...");
-    for (i, a) in (base..limit).step_by(step_size).enumerate() {
-        if i % print_step == 0 {
-            print!(".");
-        }
-        check32(a + 0x0, DRAM_TEST_PATTERN_0 | i as u32);
-        check32(a + 0x4, DRAM_TEST_PATTERN_1 | i as u32);
-        check32(a + 0x8, DRAM_TEST_PATTERN_2 | i as u32);
-        check32(a + 0xc, DRAM_TEST_PATTERN_3 | i as u32);
-    }
-    println!();
-
-    println!("DRAM test: done :)");
-}
-
 fn main() {
     let p = Peripherals::take().unwrap();
     let glb = p.GLB;
@@ -142,7 +98,7 @@ fn main() {
     init::pll();
     psram::init();
 
-    dram_test(PSRAM_BASE, PSRAM_SIZE / 64);
+    memtest::mem_test(PSRAM_BASE, PSRAM_SIZE / 64);
 
     unsafe {
         loop {
