7c89bedfed644bc2f5db86dd265337c164cb72a3
Kanak Shilledar <kanakshilledar111@protonmail.com>
2025-03-07 14:59:41 +0530

    meles/bt0: migrate dram test to lib::util
    switch to DRAM_BASE_0 instead of DRAM_BASE_4
    
    Signed-off-by: Kanak Shilledar <kanakshilledar111@protonmail.com>

--- src/mainboard/milk-v/meles/bt0/src/main.rs
+++ src/mainboard/milk-v/meles/bt0/src/main.rs
@@ -19,6 +19,7 @@ use embedded_hal_nb::serial::Write;
 use riscv::register::{marchid, mhartid, mimpid, mvendorid};
 
 use dwc3::dwc3_gadget_run;
+use oreboot_util::mem::test;
 use util::{read32, write32};
 
 mod dram;
@@ -192,51 +193,6 @@ fn copy(source: usize, target: usize, size: usize) {
     println!(" done.");
 }
 
-fn check32(addr: usize, val: u32) {
-    let v = read32(addr);
-    if v != val {
-        println!("Error @ {addr:08x}: expected {val:08x}, got {v:08x}");
-    }
-}
-
-const DRAM_TEST_PATTERN_0: u32 = 0x2233_ccee;
-const DRAM_TEST_PATTERN_1: u32 = 0x5577_aadd;
-const DRAM_TEST_PATTERN_2: u32 = 0x1144_bbff;
-const DRAM_TEST_PATTERN_3: u32 = 0x6688_9900;
-
-fn dram_test(base: usize, size: usize) {
-    let limit = base + size;
-    let step_size = 0x100;
-    // print 64 steps, which gets slower with a higher size to test
-    let print_step = size / step_size / 64;
-
-    println!("DRAM test: write patterns...");
-    for (i, a) in (base..limit).step_by(step_size).enumerate() {
-        if i % print_step == 0 {
-            print!(".");
-        }
-        write32(a + 0x0, DRAM_TEST_PATTERN_0 | i as u32);
-        write32(a + 0x4, DRAM_TEST_PATTERN_1 | i as u32);
-        write32(a + 0x8, DRAM_TEST_PATTERN_2 | i as u32);
-        write32(a + 0xc, DRAM_TEST_PATTERN_3 | i as u32);
-    }
-    println!();
-
-    println!("DRAM test: reading back...");
-    for (i, a) in (base..limit).step_by(step_size).enumerate() {
-        if i % print_step == 0 {
-            print!(".");
-        }
-        check32(a + 0x0, DRAM_TEST_PATTERN_0 | i as u32);
-        check32(a + 0x4, DRAM_TEST_PATTERN_1 | i as u32);
-        check32(a + 0x8, DRAM_TEST_PATTERN_2 | i as u32);
-        check32(a + 0xc, DRAM_TEST_PATTERN_3 | i as u32);
-    }
-    println!();
-
-    println!("DRAM test: done :)");
-}
-
 const PMP_BASE: usize = 0xff_dc02_0000;
 
 // The mask ROM configures PMP by itself.
@@ -289,7 +245,7 @@ fn main() {
     dump_pmp();
 
     dram::setup_ddr_addrmap();
-    dram_test(DRAM_BASE_4, 0x0002_0000);
+    test(DRAM_BASE_4, 0x0002_0000);
 
     unsafe {
         asm!("wfi");
