0f91df6a7b39da511d4fa92adf5668e2fcae3ebc
Daniel Maslowski <info@orangecms.org>
2025-10-05 07:12:41 +0200

    xtask/starfive/visionfive2: rework using new helpers
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- xtask/src/starfive/visionfive2.rs
+++ xtask/src/starfive/visionfive2.rs
@@ -1,8 +1,4 @@
-use std::{
-    fs,
-    path::{Path, PathBuf},
-    process,
-};
+use std::{fs, path::PathBuf, process};
 
 use fdt::Fdt;
 use log::{error, info, trace, warn};
@@ -10,8 +6,8 @@ use log::{error, info, trace, warn};
 use layoutflash::layout::{create_areas, layout_flash};
 
 use crate::util::{
-    compile_board_dt, dist_dir, find_binutils_prefix_or_fail, get_cargo_cmd_in, objcopy,
-    platform_dir,
+    compile_board_dt, dist_dir, find_binutils_prefix_or_fail, get_bin_for, get_cargo_cmd_in,
+    objcopy, platform_dir, Bin,
 };
 use crate::{Cli, Commands, Env};
 
@@ -20,13 +16,6 @@ use super::visionfive2_hdr::{spl_create_hdr, HEADER_SIZE};
 const SRAM_SIZE: usize = 0x20_0000;
 
 const ARCH: &str = "riscv64";
-const TARGET: &str = "riscv64imac-unknown-none-elf";
-
-const BT0_BIN: &str = "starfive-visionfive2-bt0.bin";
-const BT0_ELF: &str = "starfive-visionfive2-bt0";
-
-const MAIN_BIN: &str = "starfive-visionfive2-main.bin";
-const MAIN_ELF: &str = "starfive-visionfive2-main";
 
 const BOARD_DTFS: &str = "starfive-visionfive2-board.dtb";
 
@@ -34,13 +23,25 @@ const DTFS_IMAGE: &str = "starfive-visionfive2-dtfs.bin";
 
 const IMAGE: &str = "starfive-visionfive2.bin";
 
+const BT0_STAGE: &str = "bt0";
+const MAIN_STAGE: &str = "main";
+struct Stages {
+    bt0: Bin,
+    main: Bin,
+}
+
 const DIR: &str = "starfive/visionfive2";
 
 pub(crate) fn execute_command(args: &Cli, features: Vec<String>) {
+    let dir = PathBuf::from(DIR);
+    let bt0 = get_bin_for(&dir, BT0_STAGE);
+    let main = get_bin_for(&dir, MAIN_STAGE);
+    let stages = Stages { bt0, main };
+
     match args.command {
         Commands::Make => {
             info!("Build oreboot image for VisionFive2");
-            build_image(&args.env, &features);
+            build_image(&args.env, &dir, &stages, &features);
         }
         _ => {
             error!("command {:?} not implemented", args.command);
@@ -48,10 +49,10 @@ pub(crate) fn execute_command(args: &Cli, features: Vec<String>) {
     }
 }
 
-fn xtask_build_jh7110_bt0(env: &Env, features: &[String]) {
+fn xtask_build_jh7110_bt0(env: &Env, dir: &PathBuf, bin: &Bin, features: &[String]) {
     trace!("build JH7110 bt0");
     let binutils_prefix = &find_binutils_prefix_or_fail(ARCH);
-    let mut command = get_cargo_cmd_in(env, &PathBuf::from(DIR), "bt0", "build");
+    let mut command = get_cargo_cmd_in(env, dir, BT0_STAGE, "build");
     if !features.is_empty() {
         let command_line_features = features.join(",");
         trace!("append command line features: {command_line_features}");
@@ -79,13 +80,20 @@ fn xtask_build_jh7110_bt0(env: &Env, features: &[String]) {
         process::exit(1);
     }
 
-    objcopy(env, binutils_prefix, TARGET, ARCH, BT0_ELF, BT0_BIN);
+    objcopy(
+        env,
+        binutils_prefix,
+        &bin.target,
+        ARCH,
+        &bin.elf_name,
+        &bin.bin_name,
+    );
 }
 
-fn xtask_build_jh7110_main(env: &Env) {
+fn xtask_build_jh7110_main(env: &Env, dir: &PathBuf, bin: &Bin) {
     trace!("build JH7110 main");
     let binutils_prefix = &find_binutils_prefix_or_fail(ARCH);
-    let mut command = get_cargo_cmd_in(env, &PathBuf::from(DIR), "main", "build");
+    let mut command = get_cargo_cmd_in(env, dir, MAIN_STAGE, "build");
     let status = command.status().unwrap();
     trace!("cargo returned {status}");
     if !status.success() {
@@ -93,18 +101,34 @@ fn xtask_build_jh7110_main(env: &Env) {
         process::exit(1);
     }
 
-    objcopy(env, binutils_prefix, TARGET, ARCH, MAIN_ELF, MAIN_BIN);
+    objcopy(
+        env,
+        binutils_prefix,
+        &bin.target,
+        ARCH,
+        &bin.elf_name,
+        &bin.bin_name,
+    );
 }
 
-fn xtask_build_image(env: &Env) {
-    let dir = dist_dir(env, TARGET);
-    let dtfs_path = dir.join(BOARD_DTFS);
+fn xtask_build_image(env: &Env, dir: &PathBuf, stages: &Stages) {
+    let plat_dir = platform_dir(dir);
+    let target_dir = dist_dir(env, &stages.main.target);
+
+    let dtfs_path = target_dir.join(BOARD_DTFS);
+    compile_board_dt(
+        env,
+        &stages.main.target,
+        &plat_dir,
+        dtfs_path.to_str().unwrap(),
+    );
     let dtfs_file = fs::read(dtfs_path).expect("dtfs");
+
     let dtfs = Fdt::new(&dtfs_file).unwrap();
     let areas = create_areas(&dtfs).unwrap();
 
-    let dtfs_image_path = dir.join(DTFS_IMAGE);
-    if let Err(e) = layout_flash(Path::new(&dir), Path::new(&dtfs_image_path), areas) {
+    let dtfs_image_path = target_dir.join(DTFS_IMAGE);
+    if let Err(e) = layout_flash(&target_dir, &dtfs_image_path, areas) {
         error!("layoutflash fail: {e}");
         process::exit(1);
     }
@@ -117,20 +141,17 @@ fn xtask_build_image(env: &Env) {
     trace!("image size {:08x} cut down to {cut:08x}", dat.len());
     let out = spl_create_hdr(dat[HEADER_SIZE as usize..cut].to_vec());
     trace!("final size {:08x}", out.len());
-    let out_path = dir.join(IMAGE);
+    let out_path = plat_dir.join(IMAGE);
     fs::write(out_path.clone(), out).expect("writing final image");
 
     println!("======= DONE =======");
     println!("Output file: {:?}", &out_path.into_os_string());
 }
 
-fn build_image(env: &Env, features: &[String]) {
+fn build_image(env: &Env, dir: &PathBuf, stages: &Stages, features: &[String]) {
     // Build the stages - should we parallelize this?
-    xtask_build_jh7110_bt0(env, &features);
-    xtask_build_jh7110_main(env);
+    xtask_build_jh7110_bt0(env, dir, &stages.bt0, features);
+    xtask_build_jh7110_main(env, dir, &stages.main);
 
-    // dtfs
-    compile_board_dt(env, TARGET, &platform_dir(&PathBuf::from(DIR)), BOARD_DTFS);
-    // final image
-    xtask_build_image(env);
+    xtask_build_image(env, dir, stages);
 }
