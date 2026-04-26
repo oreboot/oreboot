b27042d39e1263a0a20e24b9122b2c98250ad215
Daniel Maslowski <info@orangecms.org>
2026-04-26 12:01:29 +0200

    scripts: fix size report
    We never really used this script in a long while, which broke without
    anyone noticing. It was meant to list binary sizes of 'bootblobs',
    what we now call the bt0 stage. Also include the main stage now.
    
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- scripts/generate-size-report.sh
+++ scripts/generate-size-report.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Mainboard;Size(bytes)"
-for BOOTBLOB in $(find ./target -name *-bootblob.bin)
+for BIN in $(find ./target/*/release/ -name *.bin)
 do
-  SIZE=$(stat -c"%s" $BOOTBLOB)
-  if [[ $BOOTBLOB =~ \./target/.*/([^/]+)-([^/]+)-bootblob.bin ]]; then 
+  SIZE=$(stat -c"%s" $BIN)
+  if [[ $BIN =~ \./target/.*/([^/]+)-([^/]+).bin ]]; then 
     echo "${BASH_REMATCH[1]}/${BASH_REMATCH[2]};${SIZE}"
   fi 
 done
